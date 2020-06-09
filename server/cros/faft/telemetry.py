d2cc042b45d3b47814bd071932035898d97d0f6e
Patrick Georgi <pgeorgi@google.com>
2020-06-09 18:47:02 +0200

    FAFT: Add telemetry for USB disk related issues
    There are a couple of FAFT failures related to FAFT not finding the USB
    storage. It finds no device at all, or doesn't find the partition or
    can't mount the file system.
    
    On such an error, collect some more data on what's going on.
    
    BUG=b:155774764
    TEST=local modifications to force the exception in both places led
    to logs that contained useful information.
    
    Change-Id: Iab87f413693e3b8f880398f1dc102a61bd91ecdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2238286
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Brent Peterson <brentpeterson@chromium.org>
    Reviewed-by: Dossym Nurmukhanov <dossym@chromium.org>
    Commit-Queue: Patrick Georgi <pgeorgi@chromium.org>

---
+++ server/cros/faft/telemetry.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""These are convenience functions that ideally are never run, but in case of
+problems collect additional information about some part of the system (both
+DUT and test-driving host.
+
+Given this sporadic nature, this is not wrapped in a class but merely a
+collection of functions that get the necessary inputs at the time when they're
+called and need them.
+
+The functions return a (multi-line) string that can be attached to test errors
+and similar places that end up in the logs."""
+
+
+def collect_usb_state(servo):
+    """Collect status on USB ports and attached devices, USB mass storage in
+    particular.
+
+    _servo is a server.cros.servo object that can call into the DUT and query
+    elements.
+
+    collects the DUT-side output of :
+     - `lsusb` and `lsusb -t` output to learn about the topology;
+     - `ls -l /dev/sd*` to learn which storage devices are known to the OS and
+       what partition scheme is assumed by the kernel;
+     - `fdisk -l` for the partitioning as reported in GPT/MBR
+    """
+    lsusb = servo.system_output('lsusb')
+    lsusb_t = servo.system_output('lsusb -t')
+    lssdx = servo.system_output('ls -l /dev/sd*')
+    fdisk = servo.system_output('fdisk -l')
+    return """lsusb:
+        %s
+
+        lsusb -t:
+        %s
+
+        ls -l /dev/sd*:
+        %s
+
+        fdisk -l:
+        %s
+        """ % (lsusb, lsusb_t, lssdx, fdisk)
+
+# Add more collect functions here as necessary

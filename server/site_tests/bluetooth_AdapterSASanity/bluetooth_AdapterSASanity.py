99581b0c0dd1479deac854875e18caa1c4878e9c
Shijin Abraham <shijinabraham@google.com>
2020-06-25 22:34:30 -0700

    bluetooth: Do not start chrome for SASanity tests
    Added an arg to quick_sanity_init to control starting the browser
    
    BUG=b:139074892
    TEST=Ran bluetooth_AdapterQuickSanity
    
    Change-Id: Ic76f8f85c9e613b269a7947b35411938af7613b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2269492
    Tested-by: Shijin Abraham <shijinabraham@google.com>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Commit-Queue: Shijin Abraham <shijinabraham@google.com>

--- server/site_tests/bluetooth_AdapterSASanity/bluetooth_AdapterSASanity.py
+++ server/site_tests/bluetooth_AdapterSASanity/bluetooth_AdapterSASanity.py
@@ -213,6 +213,7 @@ class bluetooth_AdapterSASanity(BluetoothAdapterQuickTests,
         @param num_iterations: the number of rounds to execute the test
         """
         # Initialize and run the test batch or the requested specific test
-        self.quick_test_init(host, use_btpeer=False, flag=flag)
+        self.quick_test_init(host, use_btpeer=False, flag=flag,
+                    start_browser=False)
         self.sa_sanity_batch_run(num_iterations, test_name)
         self.quick_test_cleanup()

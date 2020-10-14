b8f7ffe3e454fe5f3ae9a32fb2bbb8f06690ce72
Shijin Abraham <shijinabraham@google.com>
2020-10-13 23:06:57 -0700

    bluetooth: Cleanup code that handled deprecated peername
    Bluetooth peer named hostname-chameleon was used in a transition
    period in the lab, which is deprecated now. This CL removes some
    code which handled the old name
    
    Also some formatting changes from 'git cl format --no-clang-format'
    
    BUG=b:149637050
    TEST=Ran Quicksanity, single test, batch with and without enough peers
    
    Change-Id: I0fbc5fb61833330dd84ab4ba12104b5f27cc5080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2469815
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Tested-by: Shijin Abraham <shijinabraham@google.com>
    Commit-Queue: Shijin Abraham <shijinabraham@google.com>

--- server/cros/bluetooth/bluetooth_device.py
+++ server/cros/bluetooth/bluetooth_device.py
@@ -102,7 +102,7 @@ class BluetoothDevice(object):
                 self._proxy.log_message(msg)
 
             if peer:
-                for btpeer in self.host.peer_list:
+                for btpeer in self.host.btpeer_list:
                     btpeer.log_message(msg)
         except Exception as e:
             logging.error("Exception '%s' in log_message '%s'", str(e), msg)

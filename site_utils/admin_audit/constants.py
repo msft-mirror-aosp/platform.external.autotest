30519b889dff768047f3e7d9b86730507111aa96
Otabek Kasimov <otabek@google.com>
2020-12-21 21:16:37 -0800

    autotest: Set rpm_state after check RPM configs
    rpm_state can set states: MISSING_CONFIG, WRONG_CONFIG, WORKING
    
    BUG=b:159957113
    TEST=run local deployment action
    
    ./site_utils/deployment/prepare/main.py --results-dir /tr --hostname chromeos1-row4-rack8-host3 --host-info-file /tr/host_info_store/chromeos1-row4-rack8-host3.store run-pre-deploy-verification
    
    ./site_utils/deployment/prepare/main.py --results-dir /tr --hostname chromeos2-row4-rack10-host6 --host-info-file /tr/host_info_store/chromeos2-row4-rack10-host6.store run-pre-deploy-verification
    
    Change-Id: Id95e6fc278920751365729c5629accfb670aa51b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2593721
    Tested-by: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Otabek Kasimov <otabek@google.com>

--- site_utils/admin_audit/constants.py
+++ site_utils/admin_audit/constants.py
@@ -13,3 +13,19 @@ HW_STATE_UNKNOWN = 'UNKNOWN'
 # Labels for hardware parts
 DUT_STORAGE_STATE_PREFIX = 'storage_state'
 SERVO_USB_STATE_PREFIX = 'servo_usb_state'
+
+# RPM states
+RPM_STATE_LABEL_PREFIX = 'rpm_state'
+RPM_STATE_UNKNOWN = 'UNKNOWN'
+# Config not provided
+RPM_STATE_MISSING_CONFIG = 'MISSING_CONFIG'
+# Config is incorrect or not working
+RPM_STATE_WRONG_CONFIG = 'WRONG_CONFIG'
+# Config present and working as expected
+RPM_STATE_WORKING = 'WORKING'
+RPM_STATES_SUPPORTED = (
+        RPM_STATE_UNKNOWN,
+        RPM_STATE_MISSING_CONFIG,
+        RPM_STATE_WRONG_CONFIG,
+        RPM_STATE_WORKING,
+)

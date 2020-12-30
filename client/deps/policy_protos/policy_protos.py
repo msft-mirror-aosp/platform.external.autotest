5438363aea58ea171cf973ef0cab3825fe6cb40f
Miriam Polzer <mpolzer@google.com>
2020-12-30 10:33:17 +0100

    autotest: Add device_management_backend deps
    With the next uprev of policy proto files,
    device_management_backend.proto depends on protos from shell encryption
    and private membership. Add those to the list of protos to be compiled.
    
    BUG=chromium:1116244
    TEST=test_that --board=grunt <DUT> e:policy_.*
    
    Cq-Depend: chromium:2605699
    Change-Id: I82301eaded6b25af385115c72b53fb97f89466ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2606940
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Miriam Polzer <mpolzer@google.com>
    Commit-Queue: Miriam Polzer <mpolzer@google.com>

--- client/deps/policy_protos/policy_protos.py
+++ client/deps/policy_protos/policy_protos.py
@@ -25,6 +25,9 @@ PROTO_PATHS = [
         'usr/share/protofiles/device_management_backend.proto',
         'usr/share/protofiles/cloud_policy.proto',
         'usr/share/protofiles/chrome_extension_policy.proto',
+        'usr/share/protofiles/private_membership_rlwe.proto',
+        'usr/share/protofiles/private_membership.proto',
+        'usr/share/protofiles/serialization.proto',
         'usr/include/chromeos/dbus/login_manager/policy_descriptor.proto',
 ]
 

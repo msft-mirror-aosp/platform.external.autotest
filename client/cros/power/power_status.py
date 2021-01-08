09ce53ba47e8b9a28195852cff4746e42c94e57e
Todd Broch <tbroch@google.com>
2021-01-08 13:18:57 -0800

    power_status: decrease delbin charge_full_design to 96%
    BUG=b:162360640, b:177076373
    TEST=power_LoadTest.fast
    
    Change-Id: I587a21ea499e5dc0ee91511cd95a73525c8e372f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2618556
    Reviewed-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Todd Broch <tbroch@chromium.org>

--- client/cros/power/power_status.py
+++ client/cros/power/power_status.py
@@ -30,9 +30,11 @@ BatteryDataReportType = autotest_enum.AutotestEnum('CHARGE', 'ENERGY')
 
 # For devices whose full capacity is significantly lower than design full
 # capacity, scale down their design full capacity.
-BATTERY_DESIGN_FULL_SCALE = {'jinlon': 0.95, # b/161307060
-                             'berknip': 0.94, # b/172625511
-                             }
+BATTERY_DESIGN_FULL_SCALE = {
+        'berknip': 0.94,  # b/172625511
+        'delbin': 0.96,  # b/177076373
+        'jinlon': 0.95,  # b/161307060
+}
 # battery data reported at 1e6 scale
 BATTERY_DATA_SCALE = 1e6
 # number of times to retry reading the battery in the case of bad data

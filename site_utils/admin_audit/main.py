f7a88d379ce2886d7465e7c2b197c3d9fb7e2871
Otabek Kasimov <otabek@google.com>
2021-02-09 19:42:50 -0800

    autotest: Audit use recovery mode for servo
    BUG=b:168321884
    TEST=run local audit task
    
    Change-Id: Ic23403cbac79727665ddeee24235accaeb389320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2686818
    Tested-by: Otabek Kasimov <otabek@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Garry Wang <xianuowang@chromium.org>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>

--- site_utils/admin_audit/main.py
+++ site_utils/admin_audit/main.py
@@ -100,6 +100,7 @@ def main():
                 host_info_path=opts.host_info_file,
                 try_lab_servo=need_servod,
                 try_servo_repair=need_servod,
+                try_servo_recovery=need_servod,
                 servo_uart_logs_dir=servo_uart_logs_dir)
     except Exception as err:
         logging.error("fail to create host: %s", err)

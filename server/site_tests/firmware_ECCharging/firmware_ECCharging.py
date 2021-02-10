285681eb617371326799bf8eba8035e45c4df8ab
Jeremy Bettis <jbettis@google.com>
2021-02-10 09:44:07 -0700

    firmware_ECCharging: If battery full, wait for drain.
    Instead of exiting when the battery is full, drain the battery to a
    chargable level, and then resume the test.
    
    To discharge the battery, the test will request the EC to go to
    discharge mode.  Looking at all EC config files, there appear to be a
    handful of boards that have batteries, but don't support the discharge
    mode, but those boards don't exist in the test lab.
    
    BUG=b:152530996
    TEST=Ran locally with eldrid & kindred boards.
    
    Change-Id: I1f7f486d4d4749f63c6a8638b8606ff0258f3c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2687204
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Brent Peterson <brentpeterson@chromium.org>
    Reviewed-by: Brent Peterson <brentpeterson@chromium.org>
    Reviewed-by: Philip Chen <philipchen@chromium.org>

--- server/site_tests/firmware_ECCharging/firmware_ECCharging.py
+++ server/site_tests/firmware_ECCharging/firmware_ECCharging.py
@@ -3,9 +3,11 @@
 # found in the LICENSE file.
 
 import logging
+import time
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.server.cros.faft.firmware_test import FirmwareTest
+
 
 class firmware_ECCharging(FirmwareTest):
     """
@@ -20,6 +22,15 @@ class firmware_ECCharging(FirmwareTest):
     # Threshold of trickle charging current in mA
     TRICKLE_CHARGE_THRESHOLD = 100
 
+    # We wait for up to 30 minutes for the battery to allow charging.
+    DISCHARGE_TIMEOUT = 60 * 30
+
+    # The period to check battery state while discharging.
+    CHECK_BATT_STATE_WAIT = 60
+
+    # The delay to wait for the AC state to update.
+    AC_STATE_UPDATE_DELAY = 3
+
     # The dict to cache the battery information
     BATTERY_INFO = {}
 
@@ -27,11 +38,11 @@ class firmware_ECCharging(FirmwareTest):
         super(firmware_ECCharging, self).initialize(host, cmdline_args)
         # Don't bother if there is no Chrome EC.
         if not self.check_ec_capability():
-            raise error.TestNAError("Nothing needs to be tested on this device")
+            raise error.TestNAError(
+                    "Nothing needs to be tested on this device")
         # Only run in normal mode
         self.switcher.setup_mode('normal')
         self.ec.send_command("chan 0")
-
 
     def cleanup(self):
         try:
@@ -44,21 +55,29 @@ class firmware_ECCharging(FirmwareTest):
         """Get the battery info we care for this test."""
         # The battery parameters we care for this test. The order must match
         # the output of EC battery command.
-        battery_params = ['V', 'V-desired', 'I', 'I-desired', 'Charge']
+        battery_params = [
+                'V', 'V-desired', 'I', 'I-desired', 'Charging', 'Remaining'
+        ]
         regex_str_list = []
 
         for p in battery_params:
-            if p == 'Charge':
+            if p == 'Remaining':
                 regex_str_list.append(p + ':\s+(\d+)\s+')
+            elif p == 'Charging':
+                regex_str_list.append(p + r':\s+(Not Allowed|Allowed)\s+')
             else:
-                regex_str_list.append(p + ':\s+0x[0-9a-f]*\s+=\s+([0-9-]+)\s+')
+                regex_str_list.append(p +
+                                      r':\s+0x[0-9a-f]*\s+=\s+([0-9-]+)\s+')
 
         battery_regex_match = self.ec.send_command_get_output('battery',
                                                               regex_str_list)
         for i in range(len(battery_params)):
-            self.BATTERY_INFO[battery_params[i]] = int(
-                    battery_regex_match[i][1])
-
+            if battery_params[i] == 'Charging':
+                self.BATTERY_INFO[
+                        battery_params[i]] = battery_regex_match[i][1]
+            else:
+                self.BATTERY_INFO[battery_params[i]] = int(
+                        battery_regex_match[i][1])
 
     def _get_battery_desired_voltage(self):
         """Get battery desired voltage value."""
@@ -68,7 +87,6 @@ class firmware_ECCharging(FirmwareTest):
                      self.BATTERY_INFO['V-desired'])
         return self.BATTERY_INFO['V-desired']
 
-
     def _get_battery_desired_current(self):
         """Get battery desired current value."""
         if not self.BATTERY_INFO:
@@ -77,14 +95,12 @@ class firmware_ECCharging(FirmwareTest):
                      self.BATTERY_INFO['I-desired'])
         return self.BATTERY_INFO['I-desired']
 
-
     def _get_battery_actual_voltage(self):
         """Get the actual voltage from charger to battery."""
         if not self.BATTERY_INFO:
             self._update_battery_info()
         logging.info('Battery actual voltage = %d mV', self.BATTERY_INFO['V'])
         return self.BATTERY_INFO['V']
-
 
     def _get_battery_actual_current(self):
         """Get the actual current from charger to battery."""
@@ -93,36 +109,46 @@ class firmware_ECCharging(FirmwareTest):
         logging.info('Battery actual current = %d mA', self.BATTERY_INFO['I'])
         return self.BATTERY_INFO['I']
 
-
-    def _get_battery_charge(self):
-        """Get battery charge state."""
+    def _get_battery_remaining(self):
+        """Get battery charge remaining in mAh."""
         if not self.BATTERY_INFO:
             self._update_battery_info()
-        logging.info("Battery charge = %d %%", self.BATTERY_INFO['Charge'])
-        return self.BATTERY_INFO['Charge']
+        logging.info("Battery charge remaining = %d mAh",
+                     self.BATTERY_INFO['Remaining'])
+        return self.BATTERY_INFO['Remaining']
 
+    def _get_battery_charging_allowed(self):
+        """Get the battery charging state.
+
+        Returns True if charging is allowed.
+        """
+        if not self.BATTERY_INFO:
+            self._update_battery_info()
+        logging.info("Battery charging = %s", self.BATTERY_INFO['Charging'])
+        if self.BATTERY_INFO['Charging'] == 'Allowed':
+            return True
+        return False
 
     def _get_charger_target_voltage(self):
         """Get target charging voltage set in charger."""
-        voltage = int(self.ec.send_command_get_output("charger",
-                ["V_batt:\s+(\d+)\s"])[0][1])
+        voltage = int(
+                self.ec.send_command_get_output("charger",
+                                                ["V_batt:\s+(\d+)\s"])[0][1])
         logging.info("Charger target voltage = %d mV", voltage)
         return voltage
 
-
     def _get_charger_target_current(self):
         """Get target charging current set in charger."""
-        current = int(self.ec.send_command_get_output("charger",
-                ["I_batt:\s+(\d+)\s"])[0][1])
+        current = int(
+                self.ec.send_command_get_output("charger",
+                                                ["I_batt:\s+(\d+)\s"])[0][1])
         logging.info("Charger target current = %d mA", current)
         return current
-
 
     def _get_trickle_charging(self):
         """Check if we are trickle charging battery."""
         return (self._get_battery_desired_current() <
                 self.TRICKLE_CHARGE_THRESHOLD)
-
 
     def _check_target_value(self):
         """Check charger target values are correct.
@@ -136,7 +162,6 @@ class firmware_ECCharging(FirmwareTest):
         if (self._get_charger_target_current() >=
                 1.05 * self._get_battery_desired_current()):
             raise error.TestFail("Charger target current is too high.")
-
 
     def _check_actual_value(self):
         """Check actual voltage/current values are correct.
@@ -153,8 +178,9 @@ class firmware_ECCharging(FirmwareTest):
 
     def _check_if_discharge_on_ac(self):
         """Check if DUT is performing discharge on AC"""
-        match = self.ec.send_command_get_output("battery",
-                ["Status:\s*(0x[0-9a-f]+)\s", "Param flags:\s*([0-9a-f]+)\s"])
+        match = self.ec.send_command_get_output("battery", [
+                r"Status:\s*(0x[0-9a-f]+)\s", r"Param flags:\s*([0-9a-f]+)\s"
+        ])
         status = int(match[0][1], 16)
         params = int(match[1][1], 16)
 
@@ -164,14 +190,70 @@ class firmware_ECCharging(FirmwareTest):
 
         return False
 
+    def _check_battery_discharging(self):
+        """Check if AC is attached and if charge control is normal."""
+        output = self.ec.send_command_get_output(
+                "chgstate",
+                [r"ac\s*=\s*(\d)\s*", r"chg_ctl_mode\s*=\s*(\d)\s*"])
+        ac_state = int(output[0][1])
+        chg_ctl_mode = int(output[1][1])
+        if ac_state == 0:
+            return True
+        if chg_ctl_mode == 2:  # CHARGE_CONTROL_DISCHARGE
+            return True
+        return False
+
+    def _discharge_below_100(self):
+        """Remove AC power until the battery is not full."""
+        # Ask EC to drain the battery
+        self.ec.send_command("chgstate discharge on")
+        time.sleep(self.AC_STATE_UPDATE_DELAY)
+
+        # Verify discharging. Either AC off or charge control discharge is good.
+        if not self._check_battery_discharging():
+            raise error.TestFail("Battery is not discharging.")
+        logging.info(
+                "Keep discharging until the battery reports charging allowed.")
+        try:
+            deadline = time.time() + self.DISCHARGE_TIMEOUT
+            while time.time() < deadline:
+                self._update_battery_info()
+                if self._get_battery_charging_allowed():
+                    return
+                else:
+                    logging.info("Wait for the battery to discharge (%d mAh).",
+                                 self._get_battery_remaining())
+                # Run a CPU intensive program to force the battery to drain faster.
+                # Switch to servo drain after b/140965614.
+                self._client.run("stressapptest -s %d " %
+                                 self.CHECK_BATT_STATE_WAIT)
+            raise error.TestFail(
+                    "The battery does not report charging allowed "
+                    "before timeout is reached.")
+        finally:
+            self.ec.send_command("chgstate discharge off")
+            time.sleep(self.AC_STATE_UPDATE_DELAY)
+
+            # Verify AC is on and charge control is normal.
+            if self._check_battery_discharging():
+                raise error.TestFail("Fail to plug AC and enable charging.")
+            self._update_battery_info()
+
     def run_once(self):
         """Execute the main body of the test.
         """
         if not self.check_ec_capability(['battery', 'charging']):
-            raise error.TestNAError("Nothing needs to be tested on this device")
-        if self._get_battery_charge() == 100:
-            logging.info("Battery is full. Unable to test.")
-            return
+            raise error.TestNAError(
+                    "Nothing needs to be tested on this device")
+        if not self._get_battery_charging_allowed():
+            logging.info(
+                    "Battery is full. Forcing battery discharge to test charging."
+            )
+            self._discharge_below_100()
+        if not self._get_battery_charging_allowed():
+            raise error.TestFail(
+                    'Battery reports charging is not allowed, even after discharging.'
+            )
         if self._check_if_discharge_on_ac():
             logging.info("DUT is performing discharge on AC. Unable to test.")
             return
@@ -179,7 +261,9 @@ class firmware_ECCharging(FirmwareTest):
             logging.info("Trickling charging battery. Unable to test.")
             return
         if self._get_battery_actual_current() < 0:
-            raise error.TestFail("This test must be run with AC power.")
+            raise error.TestFail(
+                    "The device is not charging. Is the test run with AC plugged?"
+            )
 
         logging.info("Checking charger target values...")
         self._check_target_value()

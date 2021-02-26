f543faf43a3527124f82da462b0b706767685709
David Haddock <dhaddock@google.com>
2021-02-26 06:04:19 -0800

    Add M->N version of autoupdate_Basic.
    This will do a real update test with nebraska. It will install the
    latest serving stable version from omaha with quick provision. Then
    update to ToT with update-engine.
    
    Ideally we will put this test in the CQ as we have had several
    breakages lately (e.g crbug/1178930) that we could have prevented with
    such a test.
    
    BUG=chromium:1179009
    TEST=autoupdate_Basic.full autoupdate_Basic.m2n.full
    
    Change-Id: I7442261562e8286d089233f6a33644d401f0f6ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2722511
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- server/site_tests/autoupdate_Basic/autoupdate_Basic.py
+++ server/site_tests/autoupdate_Basic/autoupdate_Basic.py
@@ -2,22 +2,51 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from autotest_lib.client.common_lib.cros import kernel_utils
-from autotest_lib.server.cros.update_engine import update_engine_test
+import logging
+import os
 
+from autotest_lib.client.common_lib.cros import dev_server
+from autotest_lib.client.common_lib.cros import kernel_utils
+from autotest_lib.server.cros import provisioner
+from autotest_lib.server.cros.update_engine import update_engine_test
 
 class autoupdate_Basic(update_engine_test.UpdateEngineTest):
     """Performs a simple AU using Nebraska."""
     version = 1
 
-    def run_once(self, full_payload, job_repo_url=None):
+    def run_once(self, full_payload, job_repo_url=None, m2n=False):
         """
         Performs a N-to-N autoupdate with Nebraska.
 
+        @param full_payload: True for full payload, False for delta
         @param job_repo_url: A url pointing to the devserver where the autotest
             package for this build should be staged.
+        @m2n: M -> N update. This means we install the current stable version
+              of this board before updating to ToT.
 
         """
+        if m2n:
+            # Provision latest stable build for the current build.
+            board = self._host.get_board().split(':')[1]
+            channel = 'stable-channel'
+            delta_type = 'OMAHA'
+
+            stable_paygen_data = self._paygen_json_lookup(
+                    board, channel, delta_type)
+            # Combine the channel, board and OS version into a build label.
+            build_name = os.path.join(
+                    channel, board, stable_paygen_data[0]["chrome_os_version"])
+
+            # Install the matching build with quick provision.
+            autotest_devserver = dev_server.ImageServer.resolve(
+                    build_name, self._host.hostname)
+            update_url = autotest_devserver.get_update_url(build_name)
+            logging.info('Installing source image with update url: %s',
+                         update_url)
+            provisioner.ChromiumOSProvisioner(
+                    update_url, host=self._host,
+                    is_release_bucket=True).run_provision()
+
         # Get a payload to use for the test.
         payload_url = self.get_payload_for_nebraska(job_repo_url,
                                                     full_payload=full_payload)

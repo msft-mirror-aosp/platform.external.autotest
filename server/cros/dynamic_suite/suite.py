e3dda29577babec1490926ad671ba810641603ba
Jared Loucks <jaredloucks@google.com>
2021-03-30 16:22:21 -0600

    Add "calibration" autotest suite
    This suite copies the "sanity" suite as a COIL-compliant alternative
    to that suite.
    
    Change-Id: Ib9f9d8aa372a2e8d40bb3b5f7b972ea0dd1dfcb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2795871
    Commit-Queue: Jared Loucks <jaredloucks@google.com>
    Tested-by: Jared Loucks <jaredloucks@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>

--- server/cros/dynamic_suite/suite.py
+++ server/cros/dynamic_suite/suite.py
@@ -49,9 +49,11 @@ except ImportError as e:
     print('  - (not yet supported) be run after running ')
     print('    ../utils/build_externals.py')
 
-_FILE_BUG_SUITES = ['au', 'bvt', 'bvt-cq', 'bvt-inline', 'paygen_au_beta',
-                    'paygen_au_canary', 'paygen_au_dev', 'paygen_au_stable',
-                    'sanity', 'push_to_prod']
+_FILE_BUG_SUITES = [
+        'au', 'bvt', 'bvt-cq', 'bvt-inline', 'calibration', 'paygen_au_beta',
+        'paygen_au_canary', 'paygen_au_dev', 'paygen_au_stable', 'sanity',
+        'push_to_prod'
+]
 _AUTOTEST_DIR = global_config.global_config.get_config_value(
         'SCHEDULER', 'drone_installation_directory')
 
@@ -1207,7 +1209,7 @@ class _BaseSuite(object):
         # finish, we would lose the chance to report errors from the original
         # job.
         if self._has_retry(result) and rescheduled:
-             return
+            return
 
         if self._should_report(result):
             self._result_reporter.report(result)

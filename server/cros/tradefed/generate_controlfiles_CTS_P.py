510c8f8303d00aa0b15afe803b87bd4557e4e14a
Kazuhiro Inaba <kinaba@google.com>
2021-04-07 16:31:38 +0900

    cheets_CTS_P: Split CtsMediaTestCases to 32-bit and 64-bit jobs.
    To mitigate the case of hitting 16h time limit of our lab.
    
    BUG=b:182862917
    TEST=cheets_CTS_P.9.0_r15.arm.CtsMediaTestCases.32
    
    Change-Id: I781ae446a69ac0367b28e3ea60bb825cf45e0075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2809684
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>
    Auto-Submit: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Jiyoun Ha <jiyounha@chromium.org>
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>

--- server/cros/tradefed/generate_controlfiles_CTS_P.py
+++ server/cros/tradefed/generate_controlfiles_CTS_P.py
@@ -183,6 +183,7 @@ CONFIG['NEEDS_PUSH_MEDIA'] = CONFIG['MEDIA_MODULES'] + [
 ]
 CONFIG['SPLIT_BY_BITS_MODULES'] = [
         'CtsDeqpTestCases',
+        'CtsMediaTestCases',
 ]
 
 # See b/149889853. Non-media test basically does not require dynamic

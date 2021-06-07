9cb240dbaa0ff97259096d2778dcf7e0bbac4478
Shuhei Takahashi <nya@chromium.org>
2021-06-07 01:34:49 +0000

    tast: connect to the DUT by OpenSSH in case of tast failures
    Tast occasionally fails to connect to the DUT before starting to run
    tests (b/189332919). crrev.com/c/2936579 added a logic to run ping
    on tast command failures, but it did not detect any issue over the
    last weekend.
    
    This patch updates the logic to run OpenSSH instead with maximum
    verbosity to diagnose possible SSH issues.
    
    BUG=b:189332919
    TEST=./utils/unittest_suite.py -r server/site_tests/tast --debug
    
    Change-Id: I32d24cf14f677ef0fbf8a6e09cd20f1c9edd7315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2939118
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Auto-Submit: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>

--- server/site_tests/tast/tast.py
+++ server/site_tests/tast/tast.py
@@ -425,17 +425,32 @@ class tast(test.test):
                     stdout_level=logging.INFO,
                     stderr_level=logging.ERROR)
         except error.CmdError as e:
-            # Run the ping command to debug possible network problems.
+            # Run the ssh command to debug possible SSH issues.
             # TODO(b/189332919): Remove this logic once we finish debugging.
-            logging.info('Tast exited abnormally. Running ping to diagnose '
-                         'possible network issues...')
-            utils.run('ping -c 5 -n -v -w 30 %s' % self._host.hostname,
-                      ignore_status=True,
-                      stdout_tee=utils.TEE_TO_LOGS,
-                      stderr_tee=utils.TEE_TO_LOGS,
-                      stderr_is_expected=True,
-                      stdout_level=logging.INFO,
-                      stderr_level=logging.ERROR)
+            logging.info('Tast exited abnormally. Running ssh to diagnose '
+                         'possible SSH issues...')
+            utils.run(
+                    'ssh '
+                    # Enable maximum debug logging.
+                    '-vvv '
+                    # Disable connection sharing to debug connection issues.
+                    '-o ControlPath=none '
+                    # Following arguments were copied from Autotest logs.
+                    '-a -x '
+                    '-o StrictHostKeyChecking=no '
+                    '-o UserKnownHostsFile=/dev/null '
+                    '-o BatchMode=yes '
+                    '-o ConnectTimeout=10 '
+                    '-o ConnectionAttempts=3 '
+                    '-l root -p %d %s true' %
+                    (self._host.port, self._host.hostname),
+                    timeout=60,
+                    ignore_status=True,
+                    stdout_tee=utils.TEE_TO_LOGS,
+                    stderr_tee=utils.TEE_TO_LOGS,
+                    stderr_is_expected=True,
+                    stdout_level=logging.INFO,
+                    stderr_level=logging.ERROR)
             # The tast command's output generally ends with a line describing
             # the error that was encountered; include it in the first line of
             # the TestFail exception. Fall back to stderr if stdout is empty (as

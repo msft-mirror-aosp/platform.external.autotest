f82a7b9d41e9b0a7ab4679106e6f35c6418193da
Kuo Jen Wei <inker@google.com>
2021-06-08 16:14:40 +0800

    autotest: Remove media profile filter logic in camerabox test
    Remove the profile filtering logic in test and rely on logic in
    crrev.com/c/2914936 to filter profile mount to ARC++.
    
    BUG=b:187239915
    TEST=test_that --args="chart=<CHART>" <DUT>
    cheets_CTS_P.9.0_r16.arm.CtsCameraTestCases.noled.camerabox.front
    
    Change-Id: I02503498cb363c846e10a309b5b5838b0e861a88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2939120
    Tested-by: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Jasmine Chen <lnishan@google.com>
    Commit-Queue: Jasmine Chen <lnishan@google.com>

--- server/cros/camerabox_utils.py
+++ server/cros/camerabox_utils.py
@@ -6,13 +6,10 @@
 import contextlib
 import json
 import logging
-from lxml import etree
 import os
-import six
 import time
 
 from autotest_lib.client.common_lib import error, utils
-from autotest_lib.server.cros.tradefed import tradefed_chromelogin as login
 
 
 class ChartFixture:
@@ -87,8 +84,6 @@ def get_chart_address(host_address, args):
 class DUTFixture:
     """Sets up camera filter for target camera facing on DUT."""
     TEST_CONFIG_PATH = '/var/cache/camera/test_config.json'
-    CAMERA_PROFILE_PATH = ('/mnt/stateful_partition/encrypted/var/cache/camera'
-                           '/media_profiles.xml')
     CAMERA_SCENE_LOG = '/tmp/scene.jpg'
 
     def __init__(self, test, host, facing):
@@ -102,50 +97,6 @@ class DUTFixture:
         self.host.run('setenforce 0')
         yield
         self.host.run('setenforce', args=(selinux_mode, ))
-
-    def _filter_camera_profile(self, content, facing):
-        """Filter camera profile of target facing from content of camera
-        profile.
-
-        @return:
-            New camera profile with only target facing, camera ids are
-            renumbered from 0.
-        """
-        tree = etree.parse(
-                six.StringIO(content),
-                parser=etree.XMLParser(compact=False))
-        root = tree.getroot()
-        profiles = root.findall('CamcorderProfiles')
-        logging.debug('%d number of camera(s) found in camera profile',
-                      len(profiles))
-        assert 1 <= len(profiles) <= 2
-        if len(profiles) == 2:
-            cam_id = 0 if facing == 'back' else 1
-            for p in profiles:
-                if cam_id == int(p.attrib['cameraId']):
-                    p.attrib['cameraId'] = '0'
-                else:
-                    root.remove(p)
-        else:
-            with login.login_chrome(
-                    hosts=[self.host],
-                    board=self.test._get_board_name(),
-            ), self._set_selinux_permissive():
-                has_front_camera = (
-                        'feature:android.hardware.camera.front' in self.host.
-                        run_output('android-sh -c "pm list features"'))
-                logging.debug('has_front_camera=%s', has_front_camera)
-            if (facing == 'front') != has_front_camera:
-                root.remove(profiles[0])
-        return etree.tostring(
-                tree, xml_declaration=True, encoding=tree.docinfo.encoding)
-
-    def _read_file(self, filepath):
-        """Read content of filepath from host."""
-        tmp_path = os.path.join(self.test.tmpdir, os.path.basename(filepath))
-        self.host.get_file(filepath, tmp_path, delete_dest=True)
-        with open(tmp_path) as f:
-            return f.read()
 
     def _write_file(self, filepath, content, permission=None, owner=None):
         """Write content to filepath on remote host.
@@ -163,7 +114,6 @@ class DUTFixture:
 
     def initialize(self):
         """Filter out camera other than target facing on DUT."""
-        logging.info('Restart camera service with filter option')
         self._write_file(
                 self.TEST_CONFIG_PATH,
                 json.dumps({
@@ -172,12 +122,15 @@ class DUTFixture:
                         'enable_external_camera': False
                 }),
                 owner='arc-camera')
+
+        # cros_camera_service will reference the test config to filter out
+        # undesired cameras.
+        logging.info('Restart camera service with filter option')
         self.host.upstart_restart('cros-camera')
 
-        logging.info('Replace camera profile in ARC++ container')
-        profile = self._read_file(self.CAMERA_PROFILE_PATH)
-        new_profile = self._filter_camera_profile(profile, self.facing)
-        self._write_file(self.CAMERA_PROFILE_PATH, new_profile)
+        # arc_setup will reference the test config to filter out the media
+        # profile of undesired cameras.
+        logging.info('Restart ARC++ container with camera test config')
         self.host.run('restart ui')
 
     @contextlib.contextmanager

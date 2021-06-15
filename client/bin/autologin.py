9a48e080b1ec673b6dcf96c819ca660adfd65fe6
Kazuhiro Inaba <kinaba@chromium.org>
2021-06-15 16:32:17 +0000

    Revert "autologin.py: Allow a list of tabs to be provided"
    This reverts commit bc5e76c6f9e47ea7a5410ff525718549e10d650b.
    
    Reason for revert: Broke all CTS tests
    
    Original change's description:
    > autologin.py: Allow a list of tabs to be provided
    >
    > --url can be specified multiple times, and the chrome browser will open
    > all the tabs specified.
    >
    > BUG=None
    > TEST=cros deploy autotest-client;
    > TEST=ssh dut /usr/local/autotest/bin/autologin.py
    > TEST=--url chrome://flags --url chrome://version --url chrome://gpu
    >
    > Change-Id: Ib15071754108622aa95cb911fd6f35b08c298813
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2928046
    > Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    > Reviewed-by: Gregory Nisbet <gregorynisbet@google.com>
    > Tested-by: Evan Benn <evanbenn@chromium.org>
    > Commit-Queue: Evan Benn <evanbenn@chromium.org>
    
    Bug: None
    Change-Id: I6226f89e9bd18d7348349d3cc23d460b948b11b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2964416
    Auto-Submit: Kazuhiro Inaba <kinaba@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>

--- client/bin/autologin.py
+++ client/bin/autologin.py
@@ -53,7 +53,7 @@ def main(args):
                         '--feature',
                         action='append',
                         help='Enables the specified Chrome feature flag')
-    parser.add_argument('--url', help='Navigate to URL.', action='append')
+    parser.add_argument('--url', help='Navigate to URL.')
     args = parser.parse_args(args)
 
     if args.password:
@@ -79,8 +79,9 @@ def main(args):
         gaia_login=(args.username is not None),
         disable_default_apps=(not args.enable_default_apps),
         dont_override_profile=args.dont_override_profile)
-    for url in args.url:
-        tab = cr.browser.tabs.New().Navigate(url)
+    if args.url:
+        tab = cr.browser.tabs[0]
+        tab.Navigate(args.url)
 
 
 if __name__ == '__main__':

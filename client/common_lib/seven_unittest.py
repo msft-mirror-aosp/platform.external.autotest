c384c216982ad92b12b456cb3f45f929c34a6ec8
Derek Beckett <dbeckett@chromium.org>
2021-07-21 10:31:39 -0700

    [PY3][Autotest] - converting common_lib unittests to py3 compatible
    Removed mail, as the unittest imported a module which wasn't present,
    and I found no uses of it in the repo.
    minus dev_server
    
    BUG=b:192594749
    TEST=unittest_suite -r common_lib --py_version=3
    
    Change-Id: Ic58e4de88c7759539f2624ce293c490f7265b233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3043692
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Tested-by: Derek Beckett <dbeckett@chromium.org>
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Ruben Zakarian <rzakarian@chromium.org>

--- client/common_lib/seven_unittest.py
+++ client/common_lib/seven_unittest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 # Copyright (c) 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -11,7 +11,7 @@ import sys
 import tempfile
 import unittest
 
-import seven
+from autotest_lib.client.common_lib import seven
 
 
 class TestExecCompileFile(unittest.TestCase):

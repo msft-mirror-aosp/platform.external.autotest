bdf359dc689ed0fc70023bc9a3fb38c54ef2bc3c
Jae Hoon Kim <kimjae@chromium.org>
2021-07-30 21:35:50 +0000

    Reland "update_engine_test: Create stateful restore failure marker"
    This is a reland of e33bc5f28231704d95969dd7a345ba45e9d2c09a
    
    Original change's description:
    > update_engine_test: Create stateful restore failure marker
    >
    > .. So the lab provisioning can fix stateful in the case that autotest
    > restoring stateful wasn't able to. This marker will cause lab
    > proviioning to "force" install a stateful partition again.
    > Fixing the current stateful mismatch in version with OS.
    >
    > BUG=b:194760761
    > TEST=none
    >
    > Change-Id: I754e60259ab6b418b2cbd64f6999ea8883818649
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3064843
    > Tested-by: Jae Hoon Kim <kimjae@chromium.org>
    > Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    > Reviewed-by: David Haddock <dhaddock@chromium.org>
    > Reviewed-by: Xianuo Wang <xianuowang@chromium.org>
    
    Bug: b:194760761
    Change-Id: I4da771cae5e02d7c959f9629c33d741786dc8dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3076128
    Tested-by: Jae Hoon Kim <kimjae@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>

--- server/cros/update_engine/update_engine_test.py
+++ server/cros/update_engine/update_engine_test.py
@@ -71,6 +71,8 @@ class UpdateEngineTest(test.test, update_engine_util.UpdateEngineUtil):
     # Subtest to use for logging into DUTs in a test.
     _LOGIN_TEST = 'login_LoginSuccess'
 
+    _CORRUPT_STATEFUL_PATH = '/mnt/stateful_partition/.corrupt_stateful'
+
     def initialize(self, host=None, hosts=None):
         """
         Sets default variables for the test.
@@ -726,6 +728,9 @@ class UpdateEngineTest(test.test, update_engine_util.UpdateEngineUtil):
 
     def _restore_stateful(self):
         """Restore the stateful partition after a destructive test."""
+        # Fallback to lab provisioning if this function fails to restore.
+        self._run(['touch', self._CORRUPT_STATEFUL_PATH])
+
         # Stage stateful payload.
         ds_url, build = tools.get_devserver_build_from_package_url(
                 self._job_repo_url)

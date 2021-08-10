9f7558f88f435577205b26837ae4d06a674af51b
Kevin Lund <kglund@google.com>
2021-08-10 17:18:38 -0600

    autotest: start checking TCP TX throughput over 100 mbps again
    Due to a bottleneck in the testbad, we had to stop verifying any
    performance results for the TCP TX configuration that were over 100
    mbps. Since the testbeds have been converted to a three-way performance
    testing setup, we should be able to achieve 100 mbps in this
    configuration again, so remove the logic that skipped verification in
    these cases.
    
    BUG=b:172211699
    TEST=test_that $DUT network_WiFi_Perf.vht80
    Shows throughput 375.29 mbps for TCP TX
    
    Change-Id: I8c82ff88fb9da5face8ea439764e872278f0f724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3088313
    Tested-by: Kevin Lund <kglund@google.com>
    Commit-Queue: Kevin Lund <kglund@google.com>
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Arowa Suliman <arowa@chromium.org>

--- server/site_tests/network_WiFi_Perf/network_WiFi_Perf.py
+++ server/site_tests/network_WiFi_Perf/network_WiFi_Perf.py
@@ -201,19 +201,10 @@ class network_WiFi_Perf(wifi_cell_test_base.WiFiCellTestBase):
                                    higher_is_better=True,
                                    graph=ap_config_tag)
             result = netperf_runner.NetperfResult.from_samples(results)
-            # TODO(b:172211699): The Gale AP is bad at being an endpoint while simultaneously being a netperf server.
-            # The Gale CPU performance becomes a bottleneck at high throughputs. Because of that,
-            # the "tcp_rx" tests can't meet expected throughputs that are greater than 100Mbps.
-            if (config.test_type_name == netperf_runner.NetperfConfig.
-                        TEST_TYPE_TCP_MAERTS) and (expected_throughput[0] >
-                                                   100):
-                logging.info(
-                        "Can't verify any expected throughput greater than 100Mbps on the test configuration tcp_rx"
-                )
-            else:
-                self.verify_result(result, expected_throughput[0],
-                                   expected_throughput[1], config,
-                                   failed_configs, power_save, ap_config)
+
+            self.verify_result(result, expected_throughput[0],
+                               expected_throughput[1], config, failed_configs,
+                               power_save, ap_config)
             self.write_perf_keyval(result.get_keyval(
                 prefix='_'.join([ap_config_tag, config.tag])))
         if governor:

c2506e496038c4be779e1fbb3670c67ff241e8cf
Joseph Hwang <josephsih@chromium.org>
2021-09-16 15:28:30 +0800

    bluetooth: add Lint in server/cros/bluetooth
    BUG=b:200107744
    TEST=pylint passed
    
    Change-Id: Iace82e03e4319e1956dffab86fe9a8132ca74326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3164376
    Tested-by: Shyh-In Hwang <josephsih@chromium.org>
    Commit-Queue: Shyh-In Hwang <josephsih@chromium.org>
    Reviewed-by: Shijin Abraham <shijinabraham@google.com>

--- server/cros/bluetooth/bluetooth_test_utils.py
+++ server/cros/bluetooth/bluetooth_test_utils.py
@@ -1,3 +1,7 @@
+# Lint as: python2, python3
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 """Provides utilities to support bluetooth adapter tests"""
 
 from __future__ import absolute_import

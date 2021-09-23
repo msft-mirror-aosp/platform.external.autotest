e81eb377eacfb3f660ce3789e44e8cba2f6e82dc
Shijin Abraham <shijinabraham@google.com>
2021-09-23 15:40:07 -0700

    bluetooth: Fix bluetooth_PeerVerify test and add control file
    - Add a control file to run the check_rssi test individually.
    - Fix the test so that it fails with TestFail
    
    BUG=b:199529217
    TEST=Run bluetooth_PeerVerify.check_rssi test
    
    Change-Id: I68c71fb238d6f74282328d8d335a4f971946dfa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3179319
    Commit-Queue: Shijin Abraham <shijinabraham@google.com>
    Commit-Queue: Michael Sun <michaelfsun@google.com>
    Tested-by: Shijin Abraham <shijinabraham@google.com>
    Auto-Submit: Shijin Abraham <shijinabraham@google.com>
    Reviewed-by: Michael Sun <michaelfsun@google.com>

--- server/site_tests/bluetooth_PeerVerify/bluetooth_PeerVerify.py
+++ server/site_tests/bluetooth_PeerVerify/bluetooth_PeerVerify.py
@@ -15,6 +15,9 @@ import logging
 from autotest_lib.server.cros.bluetooth.bluetooth_adapter_quick_tests import (
         BluetoothAdapterQuickTests)
 
+from autotest_lib.server.cros.bluetooth.bluetooth_adapter_tests import (
+        test_retry_and_log)
+
 test_wrapper = BluetoothAdapterQuickTests.quick_test_test_decorator
 batch_wrapper = BluetoothAdapterQuickTests.quick_test_batch_decorator
 
@@ -22,21 +25,25 @@ batch_wrapper = BluetoothAdapterQuickTests.quick_test_batch_decorator
 class bluetooth_PeerVerify(BluetoothAdapterQuickTests):
     """Test to check the RSSI of btpeers."""
 
-    @test_wrapper('check_rssi_test', devices={'MOUSE': -1}, use_all_peers=True)
-    def test_check_rssi(self):
+    @test_wrapper('Check if rssi of peers > -70 ',
+                  devices={'MOUSE': -1},
+                  use_all_peers=True)
+    @test_retry_and_log(False)
+    def check_rssi(self):
         """Check if RSSI > -70. """
         try:
             rssi_list = []
+            self.result = {}
             for n, device in enumerate(self.devices['MOUSE']):
                 rssi = self.get_device_sample_rssi(device)
                 rssi_list.append(rssi)
-                logging.info('RSSI for peer %s is %s', (n, rssi))
+                logging.info('RSSI for peer %s is %s', n, rssi)
             logging.info('RSSI values are %s', rssi_list)
-            for rssi in rssi_list:
-                if rssi is None or rssi < -70:
-                    logging.info('RSSI not found or too low')
-                    return False
-            return True
+            self.results = {'rssi': rssi_list}
+            return all([
+                    True if rssi is not None and rssi > -70 else False
+                    for rssi in rssi_list
+            ])
         except Exception as e:
             logging.debug('exception in test_check_rssi %s', str(e))
             return False

95e6bd94294ee59bf52ae6c30e1ec2766fdb910f
Roman Sorokin <rsorokin@chromium.org>
2021-09-30 16:25:54 +0200

    Migrate login_LoginSuccess to python3
    BUG=chromium:1253273
    TEST=test_that --py_version=3 -b $BOARD $DUT login_LoginSuccess
    
    Change-Id: I5076da4582d0aa8ca2fe7bcd4017b6c26e884f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3196752
    Tested-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>

--- client/site_tests/login_LoginSuccess/login_LoginSuccess.py
+++ client/site_tests/login_LoginSuccess/login_LoginSuccess.py
@@ -1,8 +1,7 @@
+# Lint as: python2, python3
 # Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-from autotest_lib.client.common_lib import error
 
 from dbus.mainloop.glib import DBusGMainLoop
 

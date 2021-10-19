cd763f09b395241ebeab1c93994aee4f629709d6
Jeremy Bettis <jbettis@google.com>
2021-10-19 11:55:55 -0600

    faft: Use recovery mode instead of cold reboot
    Requesting recovery puts the AP into RO mode, but not the EC. They need
    to match. If instead you use the power_state:rec, which is the same as
    pressing Esc+Refresh+Power.
    
    BUG=b:201238494
    TEST=Ran on local hayato
    
    Change-Id: If71215bed455f865a1e41a7fb27a894d74cc0333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3232091
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Boris Mittelberg <bmbm@google.com>
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Karthikeyan Ramasubramanian <kramasub@chromium.org>

--- server/site_tests/firmware_FwScreenPressPower/firmware_FwScreenPressPower.py
+++ server/site_tests/firmware_FwScreenPressPower/firmware_FwScreenPressPower.py
@@ -105,26 +105,24 @@ class firmware_FwScreenPressPower(FirmwareTest):
         self.switcher.wait_for_client()
 
         if self.faft_config.power_button_dev_switch:
-                logging.info(
-                        "Skipping TO_NORM screen test. The power button is "
-                        "used to confirm DEV mode to NORM mode.")
+            logging.info("Skipping TO_NORM screen test. The power button is "
+                         "used to confirm DEV mode to NORM mode.")
         else:
-                logging.info(
-                        "Reboot. When the developer screen shown, press "
-                        "enter key to trigger either TO_NORM screen (new) or "
-                        "RECOVERY INSERT screen (old). Then press power button "
-                        "to make DUT shutdown.")
-                self.check_state((self.checkers.crossystem_checker, {
-                        'devsw_boot': '1',
-                        'mainfw_type': 'developer',
-                }))
-                self.switcher.simple_reboot()
-                self.run_shutdown_process(
-                        self.wait_second_screen_and_press_power,
-                        post_power_action=self.switcher.bypass_dev_mode,
-                        shutdown_timeout=self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD
-                )
-                self.switcher.wait_for_client()
+            logging.info(
+                    "Reboot. When the developer screen shown, press "
+                    "enter key to trigger either TO_NORM screen (new) or "
+                    "RECOVERY INSERT screen (old). Then press power button "
+                    "to make DUT shutdown.")
+            self.check_state((self.checkers.crossystem_checker, {
+                    'devsw_boot': '1',
+                    'mainfw_type': 'developer',
+            }))
+            self.switcher.simple_reboot()
+            self.run_shutdown_process(
+                    self.wait_second_screen_and_press_power,
+                    post_power_action=self.switcher.bypass_dev_mode,
+                    shutdown_timeout=self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)
+            self.switcher.wait_for_client()
 
         logging.info("Request recovery boot. When the RECOVERY INSERT "
                      "screen shows, press power button to make DUT shutdown.")
@@ -132,8 +130,7 @@ class firmware_FwScreenPressPower(FirmwareTest):
                 'devsw_boot': '1',
                 'mainfw_type': 'developer',
         }))
-        self.faft_client.system.request_recovery_boot()
-        self.switcher.simple_reboot('cold')
+        self.servo.set_nocheck('power_state', 'rec')
         self.run_shutdown_process(
                 self.wait_longer_fw_screen_and_press_power,
                 post_power_action=self.switcher.bypass_dev_mode,
@@ -148,8 +145,7 @@ class firmware_FwScreenPressPower(FirmwareTest):
                 'devsw_boot': '1',
                 'mainfw_type': 'developer',
         }))
-        self.faft_client.system.request_recovery_boot()
-        self.switcher.simple_reboot('cold')
+        self.servo.set_nocheck('power_state', 'rec')
         self.run_shutdown_process(
                 self.wait_yuck_screen_and_press_power,
                 post_power_action=self.switcher.bypass_dev_mode,
@@ -170,8 +166,7 @@ class firmware_FwScreenPressPower(FirmwareTest):
                 'devsw_boot': '0',
                 'mainfw_type': 'normal',
         }))
-        self.faft_client.system.request_recovery_boot()
-        self.switcher.simple_reboot('cold')
+        self.servo.set_nocheck('power_state', 'rec')
         self.run_shutdown_process(
                 self.wait_longer_fw_screen_and_press_power,
                 shutdown_timeout=self.SHORT_SHUTDOWN_CONFIRMATION_PERIOD)

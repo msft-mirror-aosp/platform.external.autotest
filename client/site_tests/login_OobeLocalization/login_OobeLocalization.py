34db4b2c68a282a1d2c8137097da819deda31067
Roman Sorokin <rsorokin@chromium.org>
2021-11-29 15:46:07 +0100

    login_OobeLocalization: Add vmodule for login_display_host_webui
    BUG=chromium:1274589
    TEST=run locally
    
    Change-Id: Ic47f450229a50f0f0210907d52a719eed679d465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3306399
    Tested-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>

--- client/site_tests/login_OobeLocalization/login_OobeLocalization.py
+++ client/site_tests/login_OobeLocalization/login_OobeLocalization.py
@@ -79,7 +79,8 @@ class login_OobeLocalization(test.test):
     def _run_with_chrome(self, func, *args):
         with chrome.Chrome(auto_login=False,
                            extra_browser_args=[
-                                   "--disable-hid-detection-on-oobe"
+                                   "--disable-hid-detection-on-oobe",
+                                   "--vmodule=login_display_host_webui=1"
                            ]) as self._chrome:
             self._chrome.browser.oobe.WaitForJavaScriptCondition(
                     "typeof Oobe == 'function' && "

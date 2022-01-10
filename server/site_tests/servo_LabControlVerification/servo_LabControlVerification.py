539b0bc70166d6fd446c05d1f83134c7a448d781
Keith Haddow <haddowk@chromium.org>
2022-01-10 14:03:56 -0800

    autotest: Port servo_LabControlVerification to Python 3
    BUG=b:209693086
    TEST=test_that --board=fizz-labstation --model=wukong 192.168.121.20 servo_LabControlVerification --args="dut_ip=192.168.121.145"
    
    Change-Id: I67f1f90ee59169fd24de02202aa65b3b1010abd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3378968
    Reviewed-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Tested-by: Keith Haddow <haddowk@chromium.org>
    Auto-Submit: Keith Haddow <haddowk@chromium.org>
    Commit-Queue: Keith Haddow <haddowk@chromium.org>

--- server/site_tests/servo_LabControlVerification/servo_LabControlVerification.py
+++ server/site_tests/servo_LabControlVerification/servo_LabControlVerification.py
@@ -1,8 +1,9 @@
+# Lint as: python2, python3
 # Copyright 2019 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Test to validate basic, critial servo controls for the lab work."""
+"""Test to validate basic, critical servo controls for the lab work."""
 
 import logging
 import os

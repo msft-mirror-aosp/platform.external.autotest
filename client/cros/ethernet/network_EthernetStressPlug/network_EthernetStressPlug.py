2d6d0be7f8c5dd5849a6bec9e79117ce075453dc
Grant Grundler <grundler@chromium.org>
2022-01-27 18:15:59 -0800

    network_EthernetStressPlug: fix GetIPAddress readability
    The previous implementation was unreadable and could not debug
    an issue when testing. Reimplemented with readable code.
    
    BUG=none
    TEST=test_that --py_version 3 100.90.25.195 network_EthernetStressPlug
           (using 1Gbps USB dongle: RTL8153)
    
    Change-Id: I0e03879dc64eb718d6361fa6bef612a7f11fe558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3558220
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Reviewed-by: Grant Grundler <grundler@chromium.org>
    Auto-Submit: Grant Grundler <grundler@chromium.org>
    Commit-Queue: Grant Grundler <grundler@chromium.org>
    Tested-by: Grant Grundler <grundler@chromium.org>

--- client/cros/ethernet/network_EthernetStressPlug/network_EthernetStressPlug.py
+++ client/cros/ethernet/network_EthernetStressPlug/network_EthernetStressPlug.py
@@ -146,15 +146,19 @@ class network_EthernetStressPlug(test.test):
 
     def GetIPAddress(self):
         """ Obtains the ipaddress of the interface. """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockfd = sock.fileno()
+        SIOCGIFADDR = 0x8915
+
+        ifreq = struct.pack('16sH14s', self.interface.encode('utf-8'),
+                            socket.AF_INET, b'\x00' * 14)
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            return socket.inet_ntoa(
-                    fcntl.ioctl(
-                            s.fileno(),
-                            0x8915,  # SIOCGIFADDR
-                            struct.pack('256s', self.interface[:15]))[20:24])
+            res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
         except:
             return None
+
+        ip = struct.unpack('16sH2x4s8x', res)[2]
+        return socket.inet_ntoa(ip)
 
     def GetEthernetStatus(self):
         """

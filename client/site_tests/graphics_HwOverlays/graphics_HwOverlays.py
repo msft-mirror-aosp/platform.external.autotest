1b92ff37adaad87d63e9f5b4dbe06393846e8a6b
Dominik Behr <dbehr@chromium.org>
2022-03-23 14:52:50 -0700

    graphics_HwOverlays: fix unitialized variable access
    Recent mass rename has created ambiguity with variable names.
    
    Fix it like https://crrev.com/c/3527478
    
    BUG=b:225092610,225645927
    TEST=test_that graphics_HwOverlays.video
    
    Change-Id: I29e2f58ec3ab8a81661c2b58a59a9dc4263e2a40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3547298
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Commit-Queue: Ilja Friedel <ihf@chromium.org>
    Tested-by: Ilja Friedel <ihf@chromium.org>

--- client/site_tests/graphics_HwOverlays/graphics_HwOverlays.py
+++ client/site_tests/graphics_HwOverlays/graphics_HwOverlays.py
@@ -11,7 +11,7 @@ from autotest_lib.client.common_lib import error
 from autotest_lib.client.cros.graphics import graphics_utils
 from autotest_lib.client.common_lib.cros import chrome
 from autotest_lib.client.cros import constants
-from autotest_lib.client.cros.multimedia import display_facade
+from autotest_lib.client.cros.multimedia import display_facade as display_facade_lib
 from autotest_lib.client.cros.multimedia import facade_resource
 
 EXTRA_BROWSER_ARGS = ['--enable-experimental-web-platform-features',
@@ -76,7 +76,7 @@ class graphics_HwOverlays(graphics_utils.GraphicsTest):
                            autotest_ext=True,
                            init_network_controller=True) as cr:
             facade = facade_resource.FacadeResource(cr)
-            display_facade = display_facade.DisplayFacadeLocal(facade)
+            display_facade = display_facade_lib.DisplayFacadeLocal(facade)
             # TODO(crbug.com/927103): Run on an external monitor if one is
             # present.
             if not display_facade.has_internal_display():

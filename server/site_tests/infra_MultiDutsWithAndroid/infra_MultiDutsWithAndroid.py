ef5050e66eb121be8f060237e07a2230bf9d09c0
Garry Wang <xianuowang@chromium.org>
2022-03-30 19:54:40 -0700

    autotest: Add adb check in infra_MultiDutsWithAndroid test
    BUG=b:187898325
    TEST=Build tryjob and run through crosfleet
    
    Change-Id: Id7a03c9455de8441153290f029a8e5dedf0096e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3561893
    Commit-Queue: Garry Wang <xianuowang@chromium.org>
    Tested-by: Garry Wang <xianuowang@chromium.org>
    Auto-Submit: Garry Wang <xianuowang@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- server/site_tests/infra_MultiDutsWithAndroid/infra_MultiDutsWithAndroid.py
+++ server/site_tests/infra_MultiDutsWithAndroid/infra_MultiDutsWithAndroid.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 from autotest_lib.client.common_lib import error
 from autotest_lib.server import test
 
@@ -12,6 +13,13 @@ class infra_MultiDutsWithAndroid(test.test):
 
     """
     version = 1
+
+    def _verify_adb(self, dut):
+        logging.info("Starting to verify basic adb actions.")
+        dut.restart_adb_server()
+        dut.ensure_device_connectivity()
+        ip_address = dut.get_wifi_ip_address()
+        logging.info("Ip address from Android device: %s", ip_address)
 
     def run_once(self, host, companions):
         """
@@ -28,6 +36,7 @@ class infra_MultiDutsWithAndroid(test.test):
                 if dut_out != 'True':
                     raise error.TestError(
                             'phone station stdout != True (got: %s)', dut_out)
+                self._verify_adb(dut)
                 android_device_tested = True
         if not android_device_tested:
             raise error.TestError(

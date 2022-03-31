0890a0a98ecfc72ecb279f352709bdf35d57b8ca
Rob Barnes <robbarnes@google.com>
2022-03-31 02:00:37 +0000

    Revert "sys_power: Exempt guybrush from firmware log errors"
    This reverts commit 9edd1aa17b677f74484e75791202cf306a3ddec9.
    
    Reason for revert: b/221231786 is fixed, this workaround can be removed.
    
    Original change's description:
    > sys_power: Exempt guybrush from firmware log errors
    >
    > Guybrush does not save AP log to cbmem during suspend.
    > So the log being checked for errors is actually from boot,
    > not suspend.
    >
    > BUG=b:221113302
    > TEST=Pass power_Resume test on guybrush
    >
    > Change-Id: I53a5733e656abb8292048d95454f40df2f64141e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3488547
    > Tested-by: Rob Barnes <robbarnes@google.com>
    > Auto-Submit: Rob Barnes <robbarnes@google.com>
    > Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    > Reviewed-by: Rob Barnes <robbarnes@google.com>
    
    Bug: b:221113302
    Change-Id: Ia258d65ab63ca4911b9b6bfecfd4ac731ce489b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3561073
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    Auto-Submit: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Anh Le <anhdle@chromium.org>

--- client/cros/power/sys_power.py
+++ client/cros/power/sys_power.py
@@ -72,10 +72,6 @@ class FirmwareError(SuspendFailure):
             # b/187561710#comment6: waive mcache error from volteer
             ('^volteer', r'mcache overflow, should increase CBFS_MCACHE size!'
              ),
-            # b/221113302: Guybrush do not save firmware log to cbmem during
-            #              resume, so all errors seen actually occurred during boot.
-            #              This workaround can be removed when b/221231786 is fixed.
-            ('^guybrush', r'.*'),
             # b/195336611: waive the eMMC error for the non-emmc sku
             ('^brya', r'No known Realtek reader found'),
             # b/233972322 : Waive this error on nissa since some nissa boards

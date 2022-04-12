d48ef6c99dfa22275433c654057341374acaa898
Struan Shrimpton <sshrimp@google.com>
2022-04-12 20:57:28 +0000

    [infra] Parse Skylab filter files based on tast_control.gni
    This will append the filter file generated in
    https://crrev.com/c/3537670 if the filter file is provided as a test_arg
    See also http://b/227381644 for the FR that would eventually replace
    this
    
    BUG=1271572
    TEST=test_that
    
    Change-Id: I01c3348b28726f5dda37810e27c1d865cd48f4cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3497997
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Reviewed-by: Jonathan Fan <jonfan@chromium.org>
    Reviewed-by: Xinan Lin <linxinan@chromium.org>
    Tested-by: Xinan Lin <linxinan@chromium.org>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>

--- server/cros/chrome_sideloader.py
+++ server/cros/chrome_sideloader.py
@@ -4,7 +4,10 @@
 
 import base64
 import logging
+import os
+import json
 import random
+import stat
 import string
 
 # Shell command to force unmount a mount point if it is mounted
@@ -182,6 +185,58 @@ def cleanup_host(host, chrome_dir, chrome_mount_point):
         host.run(['rm', '-rf', chrome_dir])
     except Exception as e:
         raise Exception('Exception during cleanup on host %s' % host, e)
+
+
+def get_tast_expr_from_file(host, args_dict, results_dir, base_path=None):
+    """
+    Get Tast expression from argument dictionary using a file.
+    If the tast_expr_file and tast_expr_key are in the dictionary returns the
+    tast expression from the file. If either/both args are not in the dict,
+    None is returned.
+    tast_expr_file expects a file containing a json dictionary which it will
+    then use tast_expr_key to pull the tast_expr.
+
+    The tast_expr_file is a json file containing a dictionary of names to tast
+    expressions like:
+
+    {
+    "default": "(\"group:mainline\" && \"dep:lacros\" && !informational)",
+    "tast_disabled_tests_from_lacros_example": "(\"group:mainline\" && \"dep:lacros\" && !informational && !\"name:lacros.Basic\")"
+    }
+
+    @param host: Host having the provisioned lacros image with the file
+    @param args_dict: Argument dictionary
+    @param results_dir: Where to store the tast_expr_file from the dut
+    @param base_path: Base path of the provisioned folder
+
+    """
+    tast_expr_file_name = args_dict.get('tast_expr_file')
+    tast_expr_key = args_dict.get('tast_expr_key')
+    if tast_expr_file_name and tast_expr_key:
+        if base_path:
+            tast_expr_file_name = os.path.join(base_path, tast_expr_file_name)
+
+        # Get the tast expr file from the provisioned lacros folder
+        if not host.path_exists(tast_expr_file_name):
+            raise Exception(
+                    'tast_expr_file: %s could not be found on the dut' %
+                    tast_expr_file_name)
+        local_file_name = os.path.join(results_dir,
+                                       os.path.basename(tast_expr_file_name))
+        st = os.stat(results_dir)
+        os.chmod(results_dir, st.st_mode | stat.S_IWRITE)
+        host.get_file(tast_expr_file_name, local_file_name, delete_dest=True)
+
+        with open(local_file_name) as tast_expr_file:
+            expr_dict = json.load(tast_expr_file)
+            expr = expr_dict.get(tast_expr_key)
+            # If both args were provided, the entry is expected in the file
+            if not expr:
+                raise Exception('tast_expr_key: %s could not be found' %
+                                tast_expr_key)
+            logging.info("tast_expr retreived from:%s", tast_expr_file)
+            return expr
+    return None
 
 
 def get_tast_expr(args_dict):
--- server/cros/chrome_sideloader_unittest.py
+++ server/cros/chrome_sideloader_unittest.py
@@ -4,12 +4,20 @@
 
 import base64
 import unittest
+import mock
 
 from autotest_lib.server.cros import chrome_sideloader
 
 
 class TestGetTastExpr(unittest.TestCase):
     """Test GetTastExpr in ChromeSideloader library."""
+
+    class MockHost:
+        def path_exists(self, path):
+            return True
+
+        def get_file(self, src, dst, delete_dest):
+            pass
 
     def testTastExpr(self):
         tast_expr = "lacros.Basic"
@@ -35,6 +43,20 @@ class TestGetTastExpr(unittest.TestCase):
         args_dict = {'tast_expr_b64': tast_expr_b64}
         with self.assertRaises(Exception):
             chrome_sideloader.get_tast_expr(args_dict)
+
+    def testTastFileWithKey(self):
+        read_data = '{"default": "(\\"group:mainline\\" && !informational)"}'
+        file_mock = mock.mock_open(read_data=read_data)
+        args_dict = {
+                'tast_expr_file': 'mocked_file',
+                'tast_expr_key': 'default'
+        }
+        with mock.patch('builtins.open', file_mock),\
+            mock.patch('os.stat'),\
+            mock.patch('os.chmod'):
+            expr = chrome_sideloader.get_tast_expr_from_file(
+                    TestGetTastExpr.MockHost(), args_dict, 'mock/path/')
+            self.assertEqual('("group:mainline" && !informational)', expr)
 
 
 def _base64_encode_str(s):

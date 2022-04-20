0d6c08fbf40a61765605dbaedfec693c764330ba
Kalin Stoyanov <kalin@chromium.org>
2022-04-19 17:54:18 -0700

    Add a model to BOARDS_WITH_TWO_INTERNAL_MICS
    BUG=b:229621831
    TEST=Ran audio_InternalCardNodes
    
    Change-Id: Ia61828d3de6601fe4ff3f4ed7663fc5e6ddc5a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3595439
    Tested-by: Kalin Stoyanov <kalin@chromium.org>
    Auto-Submit: Kalin Stoyanov <kalin@chromium.org>
    Reviewed-by: En-Shuo Hsu <enshuo@chromium.org>
    Commit-Queue: En-Shuo Hsu <enshuo@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Yu-Hsuan Hsu <yuhsuan@chromium.org>

--- client/cros/audio/audio_spec.py
+++ client/cros/audio/audio_spec.py
@@ -131,6 +131,7 @@ BOARDS_WITH_TWO_INTERNAL_MICS = [
         BoardInfo('zork', 'vilboz360', '1518534661'),
         BoardInfo('zork', 'vilboz360', '1518534662'),
         BoardInfo('keeby', 'lalala', ''),
+        BoardInfo('dedede', 'drawcia', ''),
 ]
 
 

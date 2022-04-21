fa3bcecfd3c7e055876b33ebcdb050449448f783
Yuanpeng Ni <yuanpengni@chromium.org>
2022-04-21 14:23:42 -0700

    autotest: Map updates with verifications in MiniOS update test
    Put updates and verifications in separate methods. Each update registers
    its corresponding verifications to be called later.
    
    BUG=b:224787598, b:225212823, b:225213189, b:225214307
    TEST=test_that -b ${B} ${HOST} autoupdate_MiniOS.with_dlc.delta \
    --args="running_at_desk=True"
    
    Change-Id: I488257ac37240822dc5442b7250c5779f5e93205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3601429
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Yuanpeng Ni‎ <yuanpengni@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    Tested-by: Yuanpeng Ni‎ <yuanpengni@chromium.org>

--- server/site_tests/autoupdate_MiniOS/autoupdate_MiniOS.py
+++ server/site_tests/autoupdate_MiniOS/autoupdate_MiniOS.py
@@ -19,6 +19,80 @@ class autoupdate_MiniOS(minios_test.MiniOsTest):
         self._save_extra_update_engine_logs(number_of_logs=2)
         super(autoupdate_MiniOS, self).cleanup()
 
+    def _setup_minios_update(self, has_update):
+        # Get payload URL for the MiniOS update.
+        # We'll always need a full payload for MiniOS update.
+        self._payload_urls.append(
+                self.get_payload_for_nebraska(
+                        job_repo_url=self._job_repo_url,
+                        full_payload=True,
+                        payload_type=self._PAYLOAD_TYPE.MINIOS,
+                        public_bucket=self._running_at_desk))
+
+        # MiniOS booting to be verified.
+        if has_update:
+            self._verifications.append(self._boot_minios)
+
+    def _setup_cros_update(self, has_update):
+        if has_update:
+            # Get payload URL for the platform (OS) update.
+            self._payload_urls.append(
+                    self.get_payload_for_nebraska(
+                            job_repo_url=self._job_repo_url,
+                            full_payload=self._full_payload,
+                            public_bucket=self._running_at_desk))
+
+        # Platform (OS) update to be verified.
+        self._verifications.append(lambda: self._verify_cros_update(
+                updated=has_update))
+
+    def _setup_dlc_update(self):
+        # Payload URLs for sample-dlc, a test DLC package.
+        # We'll always need a full payload for DLC installation,
+        # and optionally a delta payload if required by the test.
+        self._payload_urls.append(
+                self.get_payload_for_nebraska(
+                        job_repo_url=self._job_repo_url,
+                        full_payload=True,
+                        payload_type=self._PAYLOAD_TYPE.DLC,
+                        public_bucket=self._running_at_desk))
+        if not self._full_payload:
+            self._payload_urls.append(
+                    self.get_payload_for_nebraska(
+                            job_repo_url=self._job_repo_url,
+                            full_payload=False,
+                            payload_type=self._PAYLOAD_TYPE.DLC,
+                            public_bucket=self._running_at_desk))
+
+        # DLC update to be verified.
+        self._verifications.append(self._verify_dlc_update)
+
+    def _verify_cros_update(self, updated):
+        if updated:
+            # Verify the platform (OS) update completed successfully.
+            kernel_utils.verify_boot_expectations(self._inactive_cros,
+                                                  host=self._host)
+            rootfs_hostlog, _ = self._create_hostlog_files()
+            self.verify_update_events(self._FORCED_UPDATE, rootfs_hostlog)
+        else:
+            # Verify the Platform (OS) boot expectation unchanged.
+            kernel_utils.verify_boot_expectations(self._active_cros,
+                                                  host=self._host)
+
+    def _verify_dlc_update(self):
+        # Verify the DLC update completed successfully.
+        dlc_rootfs_hostlog, _ = self._create_dlc_hostlog_files()
+        logging.info('Checking DLC update events')
+        self.verify_update_events(
+                self._FORCED_UPDATE,
+                dlc_rootfs_hostlog[self._dlc_util._SAMPLE_DLC_ID])
+        # Verify the DLC was successfully installed.
+        self._dlc_util.remove_preloaded(self._dlc_util._SAMPLE_DLC_ID)
+        self._dlc_util.install(self._dlc_util._SAMPLE_DLC_ID,
+                               omaha_url='fake_url')
+        if not self._dlc_util.is_installed(self._dlc_util._SAMPLE_DLC_ID):
+            raise error.TestFail('Test DLC was not installed.')
+
     def run_once(self,
                  full_payload=True,
                  job_repo_url=None,
@@ -40,97 +114,51 @@ class autoupdate_MiniOS(minios_test.MiniOsTest):
                                 workstation.
 
         """
+        self._full_payload = full_payload
+        self._job_repo_url = job_repo_url
+        self._running_at_desk = running_at_desk
+
         if not with_os and with_dlc:
             logging.info("DLC only updates with the platform (OS), "
                          "automatically set with_os to True.")
             with_os = True
 
-        payload_urls = []
-        # Payload URL for the MiniOS update.
-        # We'll always need a full payload for MiniOS update.
-        payload_urls.append(
-                self.get_payload_for_nebraska(
-                        job_repo_url=job_repo_url,
-                        full_payload=True,
-                        payload_type=self._PAYLOAD_TYPE.MINIOS,
-                        public_bucket=running_at_desk))
-        if with_os:
-            # Payload URL for the platform (OS) update.
-            payload_urls.append(
-                    self.get_payload_for_nebraska(
-                            job_repo_url=job_repo_url,
-                            full_payload=full_payload,
-                            public_bucket=running_at_desk))
-        if with_dlc:
-            # Payload URLs for sample-dlc, a test DLC package.
-            # We'll always need a full payload for DLC installation,
-            # and optionally a delta payload if required by the test.
-            payload_urls.append(
-                    self.get_payload_for_nebraska(
-                            job_repo_url=job_repo_url,
-                            full_payload=True,
-                            payload_type=self._PAYLOAD_TYPE.DLC,
-                            public_bucket=running_at_desk))
-            if not full_payload:
-                payload_urls.append(
-                        self.get_payload_for_nebraska(
-                                job_repo_url=job_repo_url,
-                                full_payload=False,
-                                payload_type=self._PAYLOAD_TYPE.DLC,
-                                public_bucket=running_at_desk))
-
         # Record DUT state before the update.
-        active_cros, inactive_cros = kernel_utils.get_kernel_state(self._host)
-        active_minios, inactive_minios = kernel_utils.get_minios_priority(
-                self._host)
+        self._active_cros, self._inactive_cros \
+            = kernel_utils.get_kernel_state(self._host)
+        active_minios, inactive_minios \
+            = kernel_utils.get_minios_priority(self._host)
+
+        # MiniOS update to be verified.
+        self._verifications = [
+                lambda: kernel_utils.verify_minios_priority_after_update(
+                        self._host,
+                        expected=inactive_minios if with_os else active_minios)
+        ]
+
+        # Get payload URLs and setup tests.
+        self._payload_urls = []
+        self._setup_cros_update(has_update=with_os)
+        if with_dlc:
+            self._setup_dlc_update()
+        self._setup_minios_update(has_update=with_os)
+
 
         # Update MiniOS.
         if with_dlc:
             self._run_client_test_and_check_result(
                     'autoupdate_InstallAndUpdateDLC',
-                    payload_urls=payload_urls,
+                    payload_urls=self._payload_urls,
                     allow_failure=not with_os)
         else:
             self._run_client_test_and_check_result(
                     'autoupdate_CannedOmahaUpdate',
-                    payload_url=payload_urls,
+                    payload_url=self._payload_urls,
                     allow_failure=not with_os)
 
-        # MiniOS only updates with platform (OS) update.
         if with_os:
-            # Verify the MiniOS update completed successfully.
-            kernel_utils.verify_minios_priority_after_update(
-                    self._host, expected=inactive_minios)
-
-            # Verify the platform (OS) update completed successfully.
             self._host.reboot()
-            kernel_utils.verify_boot_expectations(inactive_cros,
-                                                  host=self._host)
-            rootfs_hostlog, _ = self._create_hostlog_files()
-            self.verify_update_events(self._FORCED_UPDATE, rootfs_hostlog)
 
-            if with_dlc:
-                # Verify the DLC update completed successfully.
-                dlc_rootfs_hostlog, _ = self._create_dlc_hostlog_files()
-                logging.info('Checking DLC update events')
-                self.verify_update_events(
-                        self._FORCED_UPDATE,
-                        dlc_rootfs_hostlog[self._dlc_util._SAMPLE_DLC_ID])
-                # Verify the DLC was successfully installed.
-                self._dlc_util.remove_preloaded(self._dlc_util._SAMPLE_DLC_ID)
-                self._dlc_util.install(self._dlc_util._SAMPLE_DLC_ID,
-                                       omaha_url='fake_url')
-                if not self._dlc_util.is_installed(
-                        self._dlc_util._SAMPLE_DLC_ID):
-                    raise error.TestFail('Test DLC was not installed.')
-
-            # Verify booting into the MiniOS.
-            logging.info("Booting into MiniOS")
-            self._boot_minios()
-        else:
-            # Expecting no update happened.
-            # Verify the MiniOS priority unchanged.
-            kernel_utils.verify_minios_priority_after_update(
-                    self._host, expected=active_minios)
-            # Verify the Platform (OS) boot expectation unchanged.
-            kernel_utils.verify_boot_expectations(active_cros, host=self._host)
+        # Verify updates.
+        for verify in self._verifications:
+            verify()

98cdc4775a443d4bff7245f447ad7a319126913c
Yuanpeng Ni <yuanpengni@chromium.org>
2022-05-02 13:27:58 -0700

    autotest: Clear exclusion prefs before and after MiniOS AU test
    Remove the exclusion pref so that it will not block other AU tests in
    the same suite from getting payload.
    
    BUG=b:225214307
    TEST=test_that ${HOST} autoupdate_MiniOS.with_exclusion.delta \
    --board=${B} --args="running_at_desk=True"
    
    Change-Id: Icc14f3241281be067a1b9d91fa8a4570a0478f8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3621938
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Yuanpeng Ni‎ <yuanpengni@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    Tested-by: Yuanpeng Ni‎ <yuanpengni@chromium.org>

--- server/site_tests/autoupdate_MiniOS/autoupdate_MiniOS.py
+++ server/site_tests/autoupdate_MiniOS/autoupdate_MiniOS.py
@@ -19,16 +19,22 @@ class autoupdate_MiniOS(minios_test.MiniOsTest):
     _MINIOS_PREFS_DIR = "minios"
 
     def initialize(self, host=None):
-        """Remove all minios prefs on the DUT before starting the test. """
+        """
+        Clear test related prefs on the DUT before starting the test.
+
+        """
         super(autoupdate_MiniOS, self).initialize(host=host)
-        self._remove_update_engine_pref(pref=self._MINIOS_PREFS_DIR,
-                                        is_dir=True)
+        self._remove_minios_update_prefs()
 
     def cleanup(self):
         super(autoupdate_MiniOS, self).cleanup()
         self._save_extra_update_engine_logs(number_of_logs=2)
-        self._remove_update_engine_pref(pref=self._MINIOS_PREFS_DIR,
-                                        is_dir=True)
+        self._remove_minios_update_prefs()
+
+    def _remove_minios_update_prefs(self):
+        for pref in ((self._EXCLUSION_PREFS_DIR, True),
+                     (self._MINIOS_PREFS_DIR, True)):
+            self._remove_update_engine_pref(pref=pref[0], is_dir=pref[1])
 
     def _setup_minios_update(self, has_update, with_exclusion=False):
         # Get payload URL for the MiniOS update.

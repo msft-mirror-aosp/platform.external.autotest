1bee2241ff9e6d24e8c856f7f54204f1b38153fe
Hsin-chen Chuang <chharry@chromium.org>
2022-05-10 18:05:38 +0800

    bluetooth: Add bluetooth_quick_tests_base in common_lib.
    This CL moved the base quick test infrastructure from server side to
    common_lib, which enables the upcoming client tests to benefit from
    the quick test framework.
    
    There is no behavioral changes.
    
    BUG=b:232038261
    TEST=bluetooth_AdapterAdvHealth.adv_peer_test
    TEST=My dummy batch that skips model, skips chipset, raises error,
    raises failure, fails normally, and passes.
    TEST=bluetooth_AdapterQuickHealth.AVL
    Commented out test_version_setup() to run test on ToT,
    3 tests failed but they seem not related to this CL;
    All logs seem good.
    
    Change-Id: Ic77cf8a34a51ab012045369606c8d4aa43edd008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3640064
    Reviewed-by: Archie Pusaka <apusaka@chromium.org>
    Tested-by: Hsin-chen Chuang <chharry@google.com>
    Reviewed-by: Shuo-Peng Liao <deanliao@chromium.org>
    Reviewed-by: Shijin Abraham <shijinabraham@google.com>
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>

--- server/cros/bluetooth/bluetooth_adapter_quick_tests.py
+++ server/cros/bluetooth/bluetooth_adapter_quick_tests.py
@@ -20,6 +20,7 @@ import time
 
 import common
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib.cros.bluetooth import bluetooth_quick_tests_base
 from autotest_lib.server import site_utils
 from autotest_lib.server.cros.bluetooth import bluetooth_peer_update
 from autotest_lib.server.cros.bluetooth import bluetooth_adapter_tests
@@ -35,37 +36,11 @@ EXPECT_PEER_WAKE_SUSPEND_SEC = 60
 EXPECT_PEER_WAKE_RESUME_BY = 30
 
 
-class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
-    """This class provide wrapper function for Bluetooth quick health test
-    batches or packages.
-    The Bluetooth quick test infrastructure provides a way to quickly run a set
-    of tests. As for today, auto-test ramp up time per test is about 90-120
-    seconds, where a typical Bluetooth test may take ~30-60 seconds to run.
+class BluetoothAdapterQuickTests(
+        bluetooth_adapter_tests.BluetoothAdapterTests,
+        bluetooth_quick_tests_base.BluetoothQuickTestsBase):
+    """Bluetooth quick test implementation for server tests."""
 
-    The quick test infra, implemented in this class, saves this huge overhead
-    by running only the minimal reset and cleanup operations required between
-    each set of tests (takes a few seconds).
-
-    This class provides wrapper functions to start and end a test, a batch or a
-    package. A batch is defined as a set of tests, preferably with a common
-    subject. A package is a set of batches.
-    This class takes care of tests, batches, and packages test results, and
-    prints out summaries to results. The class also resets and cleans up
-    required states between tests, batches and packages.
-
-    A batch can also run as a separate auto-test. There is a place holder to
-    add a way to run a specific test of a batch autonomously.
-
-    A batch can be implemented by inheriting from this class, and using its
-    wrapper functions. A package can be implemented by inheriting from a set of
-    batches.
-
-    Adding a test to one of the batches is as easy as adding a method to the
-    class of the batch.
-    """
-
-    # Some delay is needed between tests. TODO(yshavit): investigate and remove
-    TEST_SLEEP_SECS = 3
     GCS_MTBF_BUCKET = 'gs://chromeos-mtbf-bt-results/'
 
 
@@ -109,9 +84,6 @@ class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
             else:
                 logging.info('Skip RSSI check.')
 
-    def _print_delimiter(self):
-        logging.info('=======================================================')
-
     @staticmethod
     def _get_bool_arg(arg, args_dict, default_value):
         """Get the target bool argument from args_dict.
@@ -148,6 +120,9 @@ class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
                         start_browser=False,
                         floss=False):
         """Inits the test batch"""
+
+        super().quick_test_init(flag)
+
         self.host = host
         self.start_browser = start_browser
         self.use_btpeer = use_btpeer
@@ -249,28 +224,22 @@ class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
 
         self.identify_platform_failure_reasons()
 
-        self.flag = flag
-        self.test_iter = None
-
-        self.bat_tests_results = []
-        self.bat_pass_count = 0
-        self.bat_fail_count = 0
-        self.bat_testna_count = 0
-        self.bat_warn_count = 0
-        self.bat_name = None
-        self.bat_iter = None
-
-        self.pkg_tests_results = []
-        self.pkg_pass_count = 0
-        self.pkg_fail_count = 0
-        self.pkg_testna_count = 0
-        self.pkg_warn_count = 0
-        self.pkg_name = None
-        self.pkg_iter = None
-        self.pkg_is_running = False
         self.mtbf_end = False
         self.mtbf_end_lock = threading.Lock()
 
+    def quick_test_get_model_name(self):
+        """Returns the model name.
+
+           Needed by BluetoothQuickTestsBase.quick_test_test_decorator.
+        """
+        return self.get_base_platform_name()
+
+    def quick_test_get_chipset_name(self):
+        """Returns the chipset name.
+
+           Needed by BluetoothQuickTestsBase.quick_test_test_decorator.
+        """
+        return self.bluetooth_facade.get_chipset_name()
 
     @staticmethod
     def quick_test_test_decorator(test_name,
@@ -320,86 +289,20 @@ class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
                                  if this is set to true
         """
 
-        def decorator(test_method):
-            """A decorator wrapper of the decorated test_method.
-               @param test_method: the test method being decorated.
-               @returns the wrapper of the test method.
-            """
-
-            @functools.wraps(test_method)
-            def wrapper(self):
-                """A wrapper of the decorated method."""
-                # Set test name before exiting so batches correctly identify
-                # failing tests
-                self.test_name = test_name
-
-                # Check that the test is runnable in current setting
-                if not (self.flag in flags or 'All' in flags):
-                    logging.info('SKIPPING TEST %s', test_name)
-                    logging.info('flag %s not in %s', self.flag, flags)
-                    self._print_delimiter()
-                    return
-
-                # Server specific runnable check.
-                if not self.quick_test_test_runnable(supports_floss):
-                    return
-
-                try:
-                    model = self.get_base_platform_name()
-                    if model in skip_models:
-                        logging.info('SKIPPING TEST %s', test_name)
-                        raise error.TestNAError(
-                                'Test not supported on this model')
-
-                    chipset = self.bluetooth_facade.get_chipset_name()
-                    logging.debug('Bluetooth module name is %s', chipset)
-                    if chipset in skip_chipsets:
-                        logging.info('SKIPPING TEST %s on chipset %s',
-                                     test_name, chipset)
-                        raise error.TestNAError(
-                                'Test not supported on this chipset')
-
-                    # Server specific pretest checks and resets.
-                    self.quick_test_test_pretest(test_name, devices,
-                                                 use_all_peers)
-
-                    self._print_delimiter()
-                    logging.info('Starting test: %s', test_name)
-
-                    test_method(self)
-                except error.TestError as e:
-                    fail_msg = '[--- error {} ({})]'.format(
-                            test_method.__name__, str(e))
-                    logging.error(fail_msg)
-                    self.fails.append(fail_msg)
-                except error.TestFail as e:
-                    fail_msg = '[--- failed {} ({})]'.format(
-                            test_method.__name__, str(e))
-                    logging.error(fail_msg)
-                    self.fails.append(fail_msg)
-                except error.TestNAError as e:
-                    fail_msg = '[--- SKIPPED {} ({})]'.format(
-                            test_method.__name__, str(e))
-                    logging.error(fail_msg)
-                    self.fails.append(fail_msg)
-                except Exception as e:
-                    fail_msg = '[--- unknown error {} ({})]'.format(
-                            test_method.__name__, str(e))
-                    logging.exception(fail_msg)
-                    self.fails.append(fail_msg)
-
-                self.quick_test_test_log_results(
-                        model_testNA=model_testNA,
-                        model_testWarn=model_testWarn,
-                        skip_common_errors=skip_common_errors)
-
-                # Server specific posttest cleanups.
-                self.quick_test_test_posttest()
-
-            return wrapper
-
-        return decorator
-
+        base_class = bluetooth_quick_tests_base.BluetoothQuickTestsBase
+        return base_class.quick_test_test_decorator(
+                test_name,
+                flags=flags,
+                check_runnable_func=lambda self: self.quick_test_test_runnable(
+                        supports_floss),
+                pretest_func=lambda self: self.quick_test_test_pretest(
+                        test_name, devices, use_all_peers),
+                posttest_func=lambda self: self.quick_test_test_posttest(),
+                model_testNA=model_testNA,
+                model_testWarn=model_testWarn,
+                skip_models=skip_models,
+                skip_chipsets=skip_chipsets,
+                skip_common_errors=skip_common_errors)
 
     def quick_test_test_runnable(self, supports_floss):
         """Checks if the test could be run."""
@@ -484,67 +387,6 @@ class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
         time.sleep(self.TEST_SLEEP_SECS)
         self.log_message('Starting test: %s' % test_name)
 
-    def quick_test_test_log_results(self,
-                                    model_testNA=[],
-                                    model_testWarn=[],
-                                    skip_common_errors=False):
-        """Logs and tracks the test results."""
-
-        result_msgs = []
-        model = self.get_base_platform_name()
-
-        if self.test_iter is not None:
-            result_msgs += ['Test Iter: ' + str(self.test_iter)]
-
-        if self.bat_iter is not None:
-            result_msgs += ['Batch Iter: ' + str(self.bat_iter)]
-
-        if self.pkg_is_running is True:
-            result_msgs += ['Package iter: ' + str(self.pkg_iter)]
-
-        if self.bat_name is not None:
-            result_msgs += ['Batch Name: ' + self.bat_name]
-
-        if self.test_name is not None:
-            result_msgs += ['Test Name: ' + self.test_name]
-
-        result_msg = ", ".join(result_msgs)
-
-        if not bool(self.fails):
-            result_msg = 'PASSED | ' + result_msg
-            self.bat_pass_count += 1
-            self.pkg_pass_count += 1
-        # The test should be marked as TESTNA if any of the test expressions
-        # were SKIPPED (they threw their own TESTNA error) or the model is in
-        # the list of NA models (so any failure is considered NA instead)
-        elif model in model_testNA or any(['SKIPPED' in x
-                                           for x in self.fails]):
-            result_msg = 'TESTNA | ' + result_msg
-            self.bat_testna_count += 1
-            self.pkg_testna_count += 1
-        elif model in model_testWarn:
-            result_msg = 'WARN   | ' + result_msg
-            self.bat_warn_count += 1
-            self.pkg_warn_count += 1
-        # Some tests may fail due to known common failure reasons (like usb
-        # disconnect during suspend, bluetoothd crashes, etc). Skip those tests
-        # with TESTNA when that happens.
-        #
-        # This should be used sparingly because it may hide legitimate errors.
-        elif bool(self.had_known_common_failure) and skip_common_errors:
-            result_msg = 'TESTNA | ' + result_msg
-            self.bat_testna_count += 1
-            self.pkg_testna_count += 1
-        else:
-            result_msg = 'FAIL   | ' + result_msg
-            self.bat_fail_count += 1
-            self.pkg_fail_count += 1
-
-        logging.info(result_msg)
-        self._print_delimiter()
-        self.bat_tests_results.append(result_msg)
-        self.pkg_tests_results.append(result_msg)
-
     def quick_test_test_posttest(self):
         """Runs posttest cleanups."""
 
@@ -615,148 +457,6 @@ class BluetoothAdapterQuickTests(bluetooth_adapter_tests.BluetoothAdapterTests):
 
         # Close the connection between peers
         self.cleanup_bt_test(test_state='NEW')
-
-    @staticmethod
-    def quick_test_batch_decorator(batch_name):
-        """A decorator providing a wrapper to a batch.
-           Using the decorator a test batch method can implement only its
-           core tests invocations and let the decorator handle the wrapper,
-           which is taking care for whether to run a specific test or the
-           batch as a whole and and running the batch in iterations
-
-           @param batch_name: the name of the batch to log
-        """
-
-        def decorator(batch_method):
-            """A decorator wrapper of the decorated test_method.
-               @param test_method: the test method being decorated.
-               @returns the wrapper of the test method.
-            """
-
-            @functools.wraps(batch_method)
-            def wrapper(self, num_iterations=1, test_name=None):
-                """A wrapper of the decorated method.
-                  @param num_iterations: how many interations to run
-                  @param test_name: specifc test to run otherwise None to run
-                                    the whole batch
-                """
-                if test_name is not None:
-                    single_test_method = getattr(self,  test_name)
-                    for iter in range(1,num_iterations+1):
-                        self.test_iter = iter
-                        single_test_method()
-
-                    if self.fails:
-                        # If failure is marked as TESTNA, prioritize that over
-                        # a failure. Same with WARN.
-                        if self.bat_testna_count > 0:
-                            raise error.TestNAError(self.fails)
-                        elif self.bat_warn_count > 0:
-                            raise error.TestWarn(self.fails)
-                        else:
-                            raise error.TestFail(self.fails)
-                else:
-                    for iter in range(1,num_iterations+1):
-                        self.quick_test_batch_start(batch_name, iter)
-                        batch_method(self, num_iterations, test_name)
-                        self.quick_test_batch_end()
-            return wrapper
-
-        return decorator
-
-
-    def quick_test_batch_start(self, bat_name, iteration=1):
-        """Start a test batch. The method clears and set batch variables"""
-        self.bat_tests_results = []
-        self.bat_pass_count = 0
-        self.bat_fail_count = 0
-        self.bat_testna_count = 0
-        self.bat_warn_count = 0
-        self.bat_name = bat_name
-        self.bat_iter = iteration
-
-
-    def quick_test_batch_end(self):
-        """Print results summary of a test batch """
-        logging.info(
-                '%s Test Batch Summary: total pass %d, total fail %d, '
-                'warn %d, NA %d', self.bat_name, self.bat_pass_count,
-                self.bat_fail_count, self.bat_warn_count,
-                self.bat_testna_count)
-        for result in self.bat_tests_results:
-            logging.info(result)
-        self._print_delimiter();
-        if self.bat_fail_count > 0:
-            logging.error('===> Test Batch Failed! More than one failure')
-            self._print_delimiter();
-            if self.pkg_is_running is False:
-                raise error.TestFail(self.bat_tests_results)
-        elif self.bat_testna_count > 0:
-            logging.error('===> Test Batch Passed! Some TestNA results')
-            self._print_delimiter();
-            if self.pkg_is_running is False:
-                raise error.TestNAError(self.bat_tests_results)
-        elif self.bat_warn_count > 0:
-            logging.error('===> Test Batch Passed! Some WARN results')
-            self._print_delimiter();
-            if self.pkg_is_running is False:
-                raise error.TestWarn(self.bat_tests_results)
-        else:
-            logging.info('===> Test Batch Passed! zero failures')
-            self._print_delimiter();
-
-
-    def quick_test_package_start(self, pkg_name):
-        """Start a test package. The method clears and set batch variables"""
-        self.pkg_tests_results = []
-        self.pkg_pass_count = 0
-        self.pkg_fail_count = 0
-        self.pkg_name = pkg_name
-        self.pkg_is_running = True
-
-
-    def quick_test_print_summary(self):
-        """Print results summary of a test package"""
-        logging.info(
-                '%s Test Package Summary: total pass %d, total fail %d, '
-                'Warn %d, NA %d', self.pkg_name, self.pkg_pass_count,
-                self.pkg_fail_count, self.pkg_warn_count,
-                self.pkg_testna_count)
-        for result in self.pkg_tests_results:
-            logging.info(result)
-        self._print_delimiter();
-
-
-    def quick_test_package_update_iteration(self, iteration):
-        """Update state and print log per package iteration.
-           Must be called to have a proper package test result tracking.
-        """
-        self.pkg_iter = iteration
-        if self.pkg_name is None:
-            logging.error('Error: no quick package is running')
-            raise error.TestFail('Error: no quick package is running')
-        logging.info('Starting %s Test Package iteration %d',
-                     self.pkg_name, iteration)
-
-
-    def quick_test_package_end(self):
-        """Print final result of a test package"""
-        if self.pkg_fail_count > 0:
-            logging.error('===> Test Package Failed! More than one failure')
-            self._print_delimiter();
-            raise error.TestFail(self.bat_tests_results)
-        elif self.pkg_testna_count > 0:
-            logging.error('===> Test Package Passed! Some TestNA results')
-            self._print_delimiter();
-            raise error.TestNAError(self.bat_tests_results)
-        elif self.pkg_warn_count > 0:
-            logging.error('===> Test Package Passed! Some WARN results')
-            self._print_delimiter();
-            raise error.TestWarn(self.bat_tests_results)
-        else:
-            logging.info('===> Test Package Passed! zero failures')
-            self._print_delimiter();
-        self.pkg_is_running = False
 
 
     def quick_test_cleanup(self):

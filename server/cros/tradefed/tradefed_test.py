2e6155f1ea407e91bc7bfd5278d25e344af651a5
Kazuhiro Inaba <kinaba@google.com>
2022-07-25 09:04:15 +0900

    cheets_CTS_P: Use JDK9 in the lab.
    P CTS supports JDK9. (Only N- CTS required older versions.)
    Let's switch to the slightly newer JDK for using some
    commandline flags.
    
    BUG=b:237990639, b:240132080
    TEST=cheets_CTS_P.internal.arm.CtsAccelerationTestCases
    
    Change-Id: Ic86eda36143b0ee637dcf6eb43e6147482d04d35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3783387
    Auto-Submit: Kazuhiro Inaba <kinaba@chromium.org>
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@google.com>

--- server/cros/tradefed/tradefed_test.py
+++ server/cros/tradefed/tradefed_test.py
@@ -148,21 +148,20 @@ class TradefedTest(test.test):
         self._install_files(constants.SDK_TOOLS_DIR,
                             constants.SDK_TOOLS_FILES, permission)
 
-        # If use_jdk9 is set true, use jdk9 than default jdk8.
-        if use_jdk9:
-            if utils.is_in_container() and not client_utils.is_moblab():
-                logging.info('Lab: switching to JDK9')
-                try:
-                    os.environ['JAVA_HOME'] = '/usr/lib/jvm/jdk-9.0.4'
-                    os.environ['PATH'] = os.environ['JAVA_HOME']\
-                                      + '/bin:' + os.environ['PATH']
-                    logging.info(
-                            subprocess.check_output(['java', '-version'],
-                                                    stderr=subprocess.STDOUT))
-                except OSError:
-                    logging.error('Can\'t change current PATH directory')
-            else:
-                logging.info('Non-lab environment: should be using JDK9+')
+        # Always use JDK9 in the lab.
+        if utils.is_in_container() and not client_utils.is_moblab():
+            logging.info('Lab: switching to JDK9')
+            try:
+                os.environ['JAVA_HOME'] = '/usr/lib/jvm/jdk-9.0.4'
+                os.environ['PATH'] = os.environ['JAVA_HOME']\
+                                  + '/bin:' + os.environ['PATH']
+                logging.info(
+                        subprocess.check_output(['java', '-version'],
+                                                stderr=subprocess.STDOUT))
+            except OSError:
+                logging.error('Can\'t change current PATH directory')
+        else:
+            logging.info('Non-lab environment: should be using JDK9+')
 
         # TODO(kinaba): Remove the hack and fully enable the feature.
         # For release branches (Rx-yyyyy.3.0 or above), always use the

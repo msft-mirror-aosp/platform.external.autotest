893967a3a64c6707e540fa1eaae63523e1c5c76c
Manish Mandlik <mmandlik@google.com>
2022-08-02 15:33:05 -0700

    Revert "bluetooth: move advmon_fg_bg_combination_tests back to flaky test"
    This reverts commit 8c3b46e920f30ca742719df66eb45d8877d164c6.
    
    Reason for revert: Test is fixed, moving back to stable suite.
    
    BUG=b:232998815, b:219812269
    TEST=Run advmon_fg_bg_combination_tests
    
    Change-Id: Id37e8b5bec53da82742a4b508d8eb6b8413ee547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3806819
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Tested-by: Manish Mandlik <mmandlik@chromium.org>
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>

--- server/site_tests/bluetooth_AdapterAdvMonitor/bluetooth_AdapterAdvMonitor.py
+++ server/site_tests/bluetooth_AdapterAdvMonitor/bluetooth_AdapterAdvMonitor.py
@@ -72,13 +72,8 @@ class bluetooth_AdapterAdvMonitor(BluetoothAdapterQuickTests,
         self.advmon_test_multi_client()
 
 
-    # Remove flags=['Quick Health'] when this test is migrated to stable suite.
     @test_wrapper('Foreground Background Combination Tests',
-                  devices={
-                          'BLE_KEYBOARD': 1,
-                          'BLE_MOUSE': 1
-                  },
-                  flags=['Quick Health'])
+                  devices={'BLE_KEYBOARD':1, 'BLE_MOUSE':1})
     def advmon_fg_bg_combination_tests(self):
         """Tests foreground and background scanning working together."""
         self.advmon_test_fg_bg_combination()

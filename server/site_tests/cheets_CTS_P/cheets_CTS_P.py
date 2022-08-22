3b317bbc9967958376cef125910466d4c7afa863
Kazuhiro Inaba <kinaba@google.com>
2022-08-22 11:27:46 +0900

    cheets_CTS_P: Uprev DEV version to 8973215.
    To test the following two changes
    https://android-review.googlesource.com/2161658
    https://android-review.googlesource.com/2189177
    
    BUG=b:242010303, b:215446537
    TEST=cheets_CTS_P.internal.arm.waivers
    
    Change-Id: I28c900663595b7e25f2c207ddc5d9792c57b5347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3844245
    Auto-Submit: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Rintaro Kuroiwa <rkuroiwa@google.com>
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>

--- server/site_tests/cheets_CTS_P/cheets_CTS_P.py
+++ server/site_tests/cheets_CTS_P/cheets_CTS_P.py
@@ -31,7 +31,7 @@ _PUBLIC_CTS = 'https://dl.google.com/dl/android/cts/'
 _INTERNAL_CTS = 'gs://chromeos-arc-images/cts/bundle/P/'
 _PARTNER_CTS = 'gs://chromeos-partner-gts/P/'
 _OFFICIAL_ZIP_NAME = 'android-cts-9.0_r20-linux_x86-%s.zip'
-_PREVIEW_ZIP_NAME = 'android-cts-8480133-linux_x86-%s.zip'
+_PREVIEW_ZIP_NAME = 'android-cts-8973215-linux_x86-%s.zip'
 _BUNDLE_MAP = {
         (None, 'arm'): _PUBLIC_CTS + _OFFICIAL_ZIP_NAME % 'arm',
         (None, 'x86'): _PUBLIC_CTS + _OFFICIAL_ZIP_NAME % 'x86',

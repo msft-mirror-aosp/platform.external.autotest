e4025a0056b44e5930b57bd4f3ae2faf21aee828
MohammadSabri <mohammad.kh.sabri@exalt.ps>
2022-09-19 12:09:26 +0300

    bluetooth: Implements socket manager DBus client (part1)
    DBus client implementations for org.chromium.bluetooth.SocketManager
    and org.chromium.bluetooth.SocketManagerCallbacks
    
    BUG=b:247103304
    TEST=None
    
    Change-Id: I707bb45b1bbebc0818b512bf3923f35eaea10d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3902324
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Commit-Queue: Hsin-chen Chuang <chharry@google.com>
    Tested-by: Mohammad Kh Sabri <mohammad.kh.sabri@exalt.ps>
    Reviewed-by: Hsin-chen Chuang <chharry@google.com>

---
+++ client/cros/bluetooth/floss/socket_manager.py
@@ -0,0 +1,325 @@
+# Lint as:python3
+# Copyright 2022 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Client class to access the Floss socket manager interface."""
+
+from enum import IntEnum
+from gi.repository import GLib
+import logging
+import math
+import random
+
+from autotest_lib.client.cros.bluetooth.floss.observer_base import ObserverBase
+from autotest_lib.client.cros.bluetooth.floss.utils import (glib_call,
+                                                            glib_callback)
+
+
+class BtStatus(IntEnum):
+    """Bluetooth return status."""
+    SUCCESS = 0
+    FAIL = 1
+    NOT_READY = 2
+    NO_MEMORY = 3
+    BUSY = 4
+    DONE = 5
+    UNSUPPORTED = 6
+    INVALID_PARAM = 7
+    UNHANDLED = 8
+    AUTH_FAILURE = 9
+    REMOTE_DEVICE_DOWN = 10
+    AUTH_REJECTED = 11
+    JNI_ENVIRONMENT_ERROR = 12
+    JNI_THREAD_ATTACH_ERROR = 13
+    WAKE_LOCK_ERROR = 14
+
+
+class SocketType(IntEnum):
+    """Socket types."""
+    GT_SOCK_ANY = 0
+    GT_SOCK_STREAM = 1
+    GT_SOCK_DGRAM = 2
+    GT_SOCK_RAW = 3
+    GT_SOCK_RDM = 4
+    GT_SOCK_SEQPACKET = 5
+    GT_SOCK_DCCP = 6
+    GT_SOCK_PACKET = 10
+
+
+class SocketManagerCallbacks:
+    """Callbacks for the socket manager interface.
+
+    Implement this to observe these callbacks when exporting callbacks via
+    register_callback.
+    """
+
+    def on_incoming_socket_ready(self, socket, status):
+        """Called when incoming socket is ready.
+
+        @param socket: BluetoothServerSocket.
+        @param status: BtStatus.
+        """
+        pass
+
+    def on_incoming_socket_closed(self, listener_id, reason):
+        """Called when incoming socket is closed.
+
+        @param listener_id: SocketId.
+        @param reason: BtStatus.
+        """
+        pass
+
+    def on_handle_incoming_connection(self, listener_id, connection):
+        """Called when incoming connection is handled.
+
+        @param listener_id: SocketId.
+        @param connection: BluetoothSocket.
+        """
+        pass
+
+    def on_outgoing_connection_result(self, connecting_id, result, socket):
+        """Called when outgoing connection is handled.
+
+        @param connecting_id: SocketId.
+        @param result: BtStatus.
+        @param socket: BluetoothSocket.
+        """
+        pass
+
+
+class FlossSocketManagerClient(SocketManagerCallbacks):
+    """Handles method calls and callbacks from the socket manager interface."""
+
+    ADAPTER_SERVICE = 'org.chromium.bluetooth'
+    SOCKET_MANAGER_INTERFACE = 'org.chromium.bluetooth.SocketManager'
+    ADAPTER_OBJECT_PATTERN = '/org/chromium/bluetooth/hci{}/adapter'
+    SOCKET_CB_OBJ_PATTERN = '/org/chromium/bluetooth/hci{}/test_socket_client{}'
+    CB_EXPORTED_INTF = 'org.chromium.bluetooth.SocketManagerCallback'
+
+    class ExportedSocketManagerCallbacks(ObserverBase):
+        """
+        <node>
+            <interface name="org.chromium.bluetooth.SocketManagerCallback">
+                <method name="OnIncomingSocketReady">
+                    <arg type="a{sv}" name="socket" direction="in" />
+                    <arg type="u" name="status" direction="in" />
+                </method>
+                <method name="OnIncomingSocketClosed">
+                    <arg type="t" name="listener_id" direction="in" />
+                    <arg type="u" name="reason" direction="in" />
+                </method>
+                <method name="OnHandleIncomingConnection">
+                    <arg type="t" name="listener_id" direction="in" />
+                    <arg type="a{sv}" name="connection" direction="in" />
+                </method>
+                <method name="OnOutgoingConnectionResult">
+                    <arg type="t" name="connecting_id" direction="in" />
+                    <arg type="u" name="result" direction="in" />
+                    <arg type="a{sv}" name="socket" direction="in" />
+                </method>
+            </interface>
+        </node>
+        """
+
+        def __init__(self):
+            """Construct exported callbacks object."""
+            ObserverBase.__init__(self)
+
+        def OnIncomingSocketReady(self, socket, status):
+            """Handle incoming socket ready callback."""
+            for observer in self.observers.values():
+                observer.on_incoming_socket_ready(socket, status)
+
+        def OnIncomingSocketClosed(self, listener_id, reason):
+            """Handle incoming socket closed callback."""
+            for observer in self.observers.values():
+                observer.on_incoming_socket_closed(listener_id, reason)
+
+        def OnHandleIncomingConnection(self, listener_id, connection):
+            """Handle incoming socket connection callback."""
+            for observer in self.observers.values():
+                observer.on_handle_incoming_connection(listener_id, connection)
+
+        def OnOutgoingConnectionResult(self, connecting_id, result, socket):
+            """Handle outgoing socket connection callback."""
+            for observer in self.observers.values():
+                observer.on_outgoing_connection_result(connecting_id, result,
+                                                       socket)
+
+    def __init__(self, bus, hci):
+        self.bus = bus
+        self.hci = hci
+        self.callbacks = None
+        self.callback_id = None
+        self.objpath = self.ADAPTER_OBJECT_PATTERN.format(hci)
+
+    def __del__(self):
+        """Destructor"""
+        del self.callbacks
+
+    @glib_callback()
+    def on_incoming_socket_ready(self, socket, status):
+        """Handle incoming socket ready callback."""
+        logging.debug('on_incoming_socket_ready: socket: %s, status: %s',
+                      socket, status)
+
+    @glib_callback()
+    def on_incoming_socket_closed(self, listener_id, reason):
+        """Handle incoming socket closed callback."""
+        logging.debug('on_incoming_socket_closed: listener_id: %s, reason: %s',
+                      listener_id, reason)
+
+    @glib_callback()
+    def on_handle_incoming_connection(self, listener_id, connection):
+        """Handle incoming socket connection callback."""
+        logging.debug(
+                'on_handle_incoming_connection: listener_id: %s,'
+                ' connection: %s', listener_id, connection)
+
+    @glib_callback()
+    def on_outgoing_connection_result(self, connecting_id, result, socket):
+        """Handle outgoing socket connection callback."""
+        logging.debug(
+                'on_outgoing_connection_result: connecting_id: %s, '
+                'result: %s, socket: %s', connecting_id, result, socket)
+
+    def _make_dbus_device(self, address, name):
+        return {
+                'address': GLib.Variant('s', address),
+                'name': GLib.Variant('s', name)
+        }
+
+    def _make_dbus_timeout(self, timeout):
+        return {'timeout_ms': GLib.Variant('i', timeout)}
+
+    @glib_call(False)
+    def has_proxy(self):
+        """Checks whether manager proxy can be acquired."""
+        return bool(self.proxy())
+
+    def proxy(self):
+        """Gets proxy object to socket manager interface for method calls."""
+        return self.bus.get(self.ADAPTER_SERVICE,
+                            self.objpath)[self.SOCKET_MANAGER_INTERFACE]
+
+    @glib_call(False)
+    def register_callbacks(self):
+        """Registers socket manager callbacks if one doesn't already exist."""
+        # Callbacks already registered
+        if self.callbacks:
+            return True
+
+        # Generate a random number between 1-1000
+        rnumber = math.floor(random.random() * 1000 + 1)
+
+        # Create and publish callbacks
+        self.callbacks = self.ExportedSocketManagerCallbacks()
+        self.callbacks.add_observer('socket_client', self)
+        objpath = self.SOCKET_CB_OBJ_PATTERN.format(self.hci, rnumber)
+
+        self.bus.register_object(objpath, self.callbacks, None)
+
+        # Register published callbacks with adapter daemon
+        self.callback_id = self.proxy().RegisterCallback(objpath)
+        return True
+
+    @glib_call(False)
+    def listen_using_l2cap_channel(self):
+        """Listens using L2CAP channel.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+        return self.proxy().ListenUsingL2capChannel(self.callback_id)
+
+    @glib_call(False)
+    def listen_using_insecure_l2cap_channel(self):
+        """Listens using insecure L2CAP channel.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+
+        return self.proxy().ListenUsingInsecureL2capChannel(self.callback_id)
+
+    @glib_call(False)
+    def listen_using_insecure_rfcomm_with_service_record(self, name, uuid):
+        """Listens using insecure RFCOMM channel with service record.
+
+        @param name: Service name.
+        @param uuid: 128-bit service UUID.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+        return self.proxy().ListenUsingInsecureRfcommWithServiceRecord(
+                self.callback_id, name, uuid)
+
+    @glib_call(False)
+    def listen_using_rfcomm_with_service_record(self, name, uuid):
+        """Listens using RFCOMM channel with service record.
+
+        @param name: Service name.
+        @param uuid: 128-bit service UUID.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+        return self.proxy().ListenUsingRfcommWithServiceRecord(
+                self.callback_id, name, uuid)
+
+    @glib_call(False)
+    def create_insecure_l2cap_channel(self, device, psm):
+        """Creates insecure L2CAP channel.
+
+        @param device: D-bus device.
+        @param psm: Protocol Service Multiplexor.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+        return self.proxy().CreateInsecureL2capChannel(self.callback_id,
+                                                       device, psm)
+
+    @glib_call(False)
+    def create_l2cap_channel(self, device, psm):
+        """Creates L2CAP channel.
+
+        @param device: D-bus device.
+        @param psm: Protocol Service Multiplexor.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+
+        return self.proxy().CreateL2capChannel(self.callback_id, device, psm)
+
+    @glib_call(False)
+    def create_insecure_rfcomm_socket_to_service_record(self, device, uuid):
+        """Creates insecure RFCOMM socket to service record.
+
+        @param device: New D-bus device.
+        @param uuid: 128-bit service UUID.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+        return self.proxy().CreateInsecureRfcommSocketToServiceRecord(
+                self.callback_id, device, uuid)
+
+    @glib_call(False)
+    def create_rfcomm_socket_to_service_record(self, device, uuid):
+        """Creates RFCOMM socket to service record.
+
+        @param device: D-bus device.
+        @param uuid: 128-bit service UUID.
+
+        @return: SocketResult as {status:BtStatus, id:int}.
+        """
+        return self.proxy().CreateRfcommSocketToServiceRecord(
+                self.callback_id, device, uuid)
+
+    @glib_call(False)
+    def accept(self, socket_id, timeout_ms=None):
+        """Accepts socket connection.
+
+        @param socket_id: New address of the adapter.
+        @param timeout_ms: Timeout in ms.
+
+        @return: BtStatus as int.
+        """
+        timeout_ms = self._make_dbus_timeout(timeout_ms)
+        return self.proxy().Accept(self.callback_id, socket_id, timeout_ms)

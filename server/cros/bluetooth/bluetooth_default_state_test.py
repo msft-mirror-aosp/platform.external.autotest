0a4d739e96e32d4765850f93f3c90bfac8111060
LeenaBaniOdeh <leena.baniodeh@exalt.ps>
2022-09-26 11:38:02 +0300

    bluetooth: Skipping read_info() and add_device() in sa_default_state_test in floss
    Both read_info() and add_device() are control socket commands that
    need available adapter indexes while Floss removes all of them when
    the adapter is powered on, which is expected in floss, as a result
    we need to skip it.
    
    BUG=b:248549375
    TEST=test_that -b<board name> <dut ip> bluetooth_AdapterSAHealth
    
    Change-Id: Ia1502a1809753460c41384ed9e82ecd4c86b7c60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3916219
    Reviewed-by: Dean Liao <deanliao@chromium.org>
    Tested-by: Dean Liao <deanliao@chromium.org>
    Reviewed-by: Hsin-chen Chuang <chharry@google.com>
    Commit-Queue: Leena BaniOdeh <leena.baniodeh@exalt.ps>

--- server/cros/bluetooth/bluetooth_default_state_test.py
+++ server/cros/bluetooth/bluetooth_default_state_test.py
@@ -16,15 +16,12 @@ from autotest_lib.server.cros.bluetooth import bluetooth_adapter_tests
 DEVICE_ADDRESS = '01:02:03:04:05:06'
 ADDRESS_TYPE = 0
 
+
 class bluetooth_Health_DefaultStateTest(
         bluetooth_adapter_tests.BluetoothAdapterTests):
-    """
-    This class implements the default state test
-    and all the helper functions it needs.
-    """
+    """This class implements the default state test and helper functions."""
+
     version = 1
-
-
 
     def compare_property(self, bluez_property, mgmt_setting, current_settings):
         """ Compare bluez property value and Kernel property
@@ -47,13 +44,12 @@ class bluetooth_Health_DefaultStateTest(
         # BR/EDR Allowlist. When this is supported the 'connectable' setting
         # remains unset and instead page scan is managed by the kernel based
         # on whether or not a BR/EDR device is in the allowlist.
-        ( commands, events ) = self.read_supported_commands()
+        (commands, events) = self.read_supported_commands()
         supports_add_device = bluetooth_socket.MGMT_OP_ADD_DEVICE in commands
 
         # Read the initial state of the adapter. Verify that it is powered down.
-        ( address, bluetooth_version, manufacturer_id,
-                supported_settings, current_settings, class_of_device,
-                name, short_name ) = self.read_info()
+        (address, bluetooth_version, manufacturer_id, supported_settings,
+         current_settings, class_of_device, name, short_name) = self.read_info()
         self.log_settings('Initial state', current_settings)
 
         if current_settings & bluetooth_socket.MGMT_SETTING_POWERED:
@@ -66,19 +62,27 @@ class bluetooth_Health_DefaultStateTest(
 
         # Verify that the Bluetooth Daemon sees that it is also powered down,
         # non-discoverable and not discovering devices.
-        bluez_properties = self.get_adapter_properties()
 
-        if bluez_properties['Powered']:
-            raise error.TestFail('Bluetooth daemon Powered property does not '
-                                 'match kernel while powered off')
-        if not self.compare_property(bluez_properties['Discoverable'],
-                                     bluetooth_socket.MGMT_SETTING_DISCOVERABLE,
-                                     current_settings):
-            raise error.TestFail('Bluetooth daemon Discoverable property '
-                                 'does not match kernel while powered off')
-        if bluez_properties['Discovering']:
-            raise error.TestFail('Bluetooth daemon believes adapter is '
-                                 'discovering while powered off')
+        if not self.floss:
+            bluez_properties = self.get_adapter_properties()
+
+            if bluez_properties['Powered']:
+                raise error.TestFail(
+                        'Bluetooth daemon Powered property does not '
+                        'match kernel while powered off')
+            if not self.compare_property(
+                    bluez_properties['Discoverable'],
+                    bluetooth_socket.MGMT_SETTING_DISCOVERABLE,
+                    current_settings):
+                raise error.TestFail('Bluetooth daemon Discoverable property '
+                                     'does not match kernel while powered off')
+            if bluez_properties['Discovering']:
+                raise error.TestFail('Bluetooth daemon believes adapter is '
+                                     'discovering while powered off')
+
+        else:
+            if self.bluetooth_facade.is_bluetoothd_proxy_valid():
+                raise error.TestFail('Bluetooth daemon is still powered')
 
         # Compare with the raw HCI state of the adapter as well, this should
         # be just not "UP", otherwise something deeply screwy is happening.
@@ -92,11 +96,12 @@ class bluetooth_Health_DefaultStateTest(
         # Power on the adapter, then read the state again. Verify that it is
         # powered up, pairable, but not discoverable.
         self.test_power_on_adapter()
-        current_settings = self.read_info()[4]
-        self.log_settings("Powered up", current_settings)
+        if not self.floss:
+            current_settings = self.read_info()[4]
+            self.log_settings("Powered up", current_settings)
 
-        if not current_settings & bluetooth_socket.MGMT_SETTING_POWERED:
-            raise error.TestFail('Bluetooth adapter is not powered')
+            if not current_settings & bluetooth_socket.MGMT_SETTING_POWERED:
+                raise error.TestFail('Bluetooth adapter is not powered')
 
         # If the kernel does not supports the BR/EDR allowlist, the adapter
         # should be generically connectable;
@@ -110,23 +115,35 @@ class bluetooth_Health_DefaultStateTest(
         # Verify that the Bluetooth Daemon sees the same state as the kernel
         # and that it's not discovering.
         bluez_properties = self.get_adapter_properties()
+        if not self.floss:
+            if not bluez_properties['Powered']:
+                raise error.TestFail(
+                        'Bluetooth daemon Powered property does not '
+                        'match kernel while powered on')
+            if not self.compare_property(
+                    bluez_properties['Pairable'],
+                    bluetooth_socket.MGMT_SETTING_PAIRABLE, current_settings):
+                raise error.TestFail(
+                        'Bluetooth daemon Pairable property does not '
+                        'match kernel while powered on')
+            if not self.compare_property(
+                    bluez_properties['Discoverable'],
+                    bluetooth_socket.MGMT_SETTING_DISCOVERABLE,
+                    current_settings):
+                raise error.TestFail('Bluetooth daemon Discoverable property '
+                                     'does not match kernel while powered on')
 
-        if not bluez_properties['Powered']:
-            raise error.TestFail('Bluetooth daemon Powered property does not '
-                                 'match kernel while powered on')
-        if not self.compare_property(bluez_properties['Pairable'],
-                                     bluetooth_socket.MGMT_SETTING_PAIRABLE,
-                                     current_settings):
-            raise error.TestFail('Bluetooth daemon Pairable property does not '
-                                 'match kernel while powered on')
-        if not self.compare_property(bluez_properties['Discoverable'],
-                                     bluetooth_socket.MGMT_SETTING_DISCOVERABLE,
-                                     current_settings):
-            raise error.TestFail('Bluetooth daemon Discoverable property '
-                                 'does not match kernel while powered on')
-        if bluez_properties['Discovering']:
-            raise error.TestFail('Bluetooth daemon believes adapter is '
-                                 'discovering while powered on')
+            if bluez_properties['Discovering']:
+                raise error.TestFail('Bluetooth daemon believes adapter is '
+                                     'discovering while powered on')
+
+        else:
+            if not self.bluetooth_facade.is_bluetoothd_proxy_valid():
+                raise error.TestFail('Bluetooth daemon is not powered')
+
+            if self.bluetooth_facade.is_discovering():
+                raise error.TestFail('Bluetooth daemon believes adapter is '
+                                     'discovering while powered on')
 
         # Compare with the raw HCI state of the adapter while powered up as
         # well.
@@ -139,16 +156,16 @@ class bluetooth_Health_DefaultStateTest(
         if not flags & bluetooth_socket.HCI_RUNNING:
             raise error.TestFail('HCI RUNNING flag does not match kernel while '
                                  'powered on')
-        if bool(flags & bluetooth_socket.HCI_ISCAN) != \
-            bool(bluez_properties['Discoverable']):
+        if bool(flags & bluetooth_socket.HCI_ISCAN) != bool(
+                bluez_properties['Discoverable']):
             raise error.TestFail('HCI ISCAN flag does not match kernel while '
                                  'powered on')
         if flags & bluetooth_socket.HCI_INQUIRY:
             raise error.TestFail('HCI INQUIRY flag does not match kernel while '
                                  'powered on')
 
-        # If the kernel does not supports the BR/EDR allowlist, the adapter
-        # should generically connectable, so should it should be in PSCAN
+        # If the kernel does not support the BR/EDR allowlist, the adapter
+        # should generically connectable, so it should be in PSCAN
         # mode. This matches the management API "connectable" setting so far.
         if not supports_add_device:
             if not flags & bluetooth_socket.HCI_PSCAN:
@@ -159,15 +176,16 @@ class bluetooth_Health_DefaultStateTest(
         # from the kernel BR/EDR allowlist. The management API "connectable"
         # setting should remain off, but we should be able to see the PSCAN
         # flag come and go.
-        if supports_add_device:
-            # If PSCAN is currently on then device is CONNECTABLE
+        if supports_add_device and not self.floss:
+            # If PSCAN is currently on, then device is CONNECTABLE
             # or a previous add device which was not removed.
             # Turn on and off DISCOVERABLE to turn off CONNECTABLE and
             # PSCAN
             if flags & bluetooth_socket.HCI_PSCAN:
-                if not (current_settings &
-                        bluetooth_socket.MGMT_SETTING_CONNECTABLE):
+                if not (current_settings
+                        & bluetooth_socket.MGMT_SETTING_CONNECTABLE):
                     raise error.TestFail('PSCAN on but device not CONNECTABLE')
+
                 logging.debug('Toggle Discoverable to turn off CONNECTABLE')
                 self.test_discoverable()
                 self.test_nondiscoverable()
@@ -185,8 +203,7 @@ class bluetooth_Health_DefaultStateTest(
             # Wait for a few seconds before reading the settings
             time.sleep(3)
             current_settings = self.read_info()[4]
-            self.log_settings("After add device",
-                                              current_settings)
+            self.log_settings("After add device", current_settings)
 
             flags = self.get_dev_info()[3]
             self.log_flags('After add device', flags)
@@ -196,6 +213,7 @@ class bluetooth_Health_DefaultStateTest(
                 self.log_settings("current settings", current_settings)
                 raise error.TestFail(
                     'Bluetooth adapter settings changed after add device')
+
             if not flags & bluetooth_socket.HCI_PSCAN:
                 raise error.TestFail('HCI PSCAN flag not set after add device')
 
@@ -203,7 +221,8 @@ class bluetooth_Health_DefaultStateTest(
             self.remove_device(DEVICE_ADDRESS, ADDRESS_TYPE)
 
             # PSCAN is still enabled for a few seconds after remove device
-            # on older devices. Wait for few second before reading the settigs
+            # on older devices. Wait for few second before reading the
+            # settings.
             time.sleep(3)
             current_settings = self.read_info()[4]
             self.log_settings("After remove device", current_settings)
@@ -213,9 +232,26 @@ class bluetooth_Health_DefaultStateTest(
 
             if current_settings != previous_settings:
                 raise error.TestFail(
-                    'Bluetooth adapter settings changed after remove device')
+                    'Bluetooth adapter settings changed after remove device'
+                )
+
             if flags & bluetooth_socket.HCI_PSCAN:
                 raise error.TestFail('HCI PSCAN flag set after remove device')
+
+        elif supports_add_device and self.floss:
+            # If PSCAN is currently on, then device is CONNECTABLE
+            # or a previous add device which was not removed.
+            # Turn on and off DISCOVERABLE to turn off CONNECTABLE and
+            # PSCAN
+            if flags & bluetooth_socket.HCI_PSCAN:
+                logging.debug('Toggle Discoverable to turn off CONNECTABLE')
+                self.test_discoverable()
+                self.test_nondiscoverable()
+                flags = self.get_dev_info()[3]
+                self.log_flags('Discoverability Toggled', flags)
+                if flags & bluetooth_socket.HCI_PSCAN:
+                    raise error.TestFail(
+                        'PSCAN on after toggling DISCOVERABLE')
 
         # Finally power off the adapter again, and verify that the adapter has
         # returned to powered down.
@@ -227,19 +263,25 @@ class bluetooth_Health_DefaultStateTest(
             raise error.TestFail('Bluetooth adapter is powered after power off')
 
         # Verify that the Bluetooth Daemon sees the same state as the kernel.
-        bluez_properties = self.get_adapter_properties()
+        if not self.floss:
+            bluez_properties = self.get_adapter_properties()
 
-        if bluez_properties['Powered']:
-            raise error.TestFail('Bluetooth daemon Powered property does not '
-                                 'match kernel after power off')
-        if not self.compare_property(bluez_properties['Discoverable'],
-                                     bluetooth_socket.MGMT_SETTING_DISCOVERABLE,
-                                     current_settings):
-            raise error.TestFail('Bluetooth daemon Discoverable property '
-                                 'does not match kernel after off')
-        if bluez_properties['Discovering']:
-            raise error.TestFail('Bluetooth daemon believes adapter is '
-                                 'discovering after power off')
+            if bluez_properties['Powered']:
+                raise error.TestFail(
+                        'Bluetooth daemon Powered property does not '
+                        'match kernel after power off')
+            if not self.compare_property(
+                    bluez_properties['Discoverable'],
+                    bluetooth_socket.MGMT_SETTING_DISCOVERABLE,
+                    current_settings):
+                raise error.TestFail('Bluetooth daemon Discoverable property '
+                                     'does not match kernel after off')
+            if bluez_properties['Discovering']:
+                raise error.TestFail('Bluetooth daemon believes adapter is '
+                                     'discovering after power off')
+        else:
+            if self.bluetooth_facade.is_bluetoothd_proxy_valid():
+                raise error.TestFail('Bluetooth daemon is still powered')
 
         # And one last comparison with the raw HCI state of the adapter.
         flags = self.get_dev_info()[3]

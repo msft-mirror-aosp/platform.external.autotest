6c0e3192e219f1553976833e46114ec9deeaade9
Andrew Luo <aluo@google.com>
2022-10-26 12:20:41 -0700

    Ti50: Run firmware.Ti50CCDUpdate from test_that.
    BUG=b:256646683
    BUG=b:256627846
    TEST=DEBUGGER_SERIAL=<serial> GSC_SERIAL=<serial> python -m utils.start_gsc_devboard_host
    TEST=test_that --fast --args "ti50_build=gs://chromeos-releases/firmware-ti50-postsubmit/<build> service_ip=localhost service_port=39999" localhost:2222 tast.firmware-Ti50CCDUpdate
    
    Change-Id: Ia24afe31adf428996033b94590c4b9f8337c246f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3994948
    Commit-Queue: Andrew Luo <aluo@chromium.org>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Tested-by: Andrew Luo <aluo@chromium.org>
    Auto-Submit: Andrew Luo <aluo@chromium.org>

--- server/hosts/gsc_devboard_host.py
+++ server/hosts/gsc_devboard_host.py
@@ -9,12 +9,28 @@
 
 import contextlib
 import logging
+import os
+import time
+
 try:
     import docker
 except ImportError:
     logging.info("Docker API is not installed in this environment")
 
-DOCKER_IMAGE = "gcr.io/satlab-images/gsc_dev_board:release"
+from autotest_lib.server.hosts import remote
+
+# Create this file in the chroot home directory
+# containing the image to use in place of the default.
+SERVICE_IMAGE_OVERRIDE = "gsc_dev_board_override"
+
+# Create this file in the chroot home directory
+# containing the image token file to enable image pull.
+# gcloud auth print-access-token > /chroot/home/${USER}/gsc_dev_board_token
+SERVICE_IMAGE_TOKEN_FILE = "gsc_dev_board_token"
+
+SERVICE_IMAGE_DEFAULT = "gcr.io/satlab-images/gsc_dev_board:release"
+
+SERVICE_START_TIMEOUT = 5
 
 SATLAB_DOCKER_HOST = 'tcp://192.168.231.1:2375'
 LOCAL_DOCKER_HOST = 'tcp://127.0.0.1:2375'
@@ -23,9 +39,10 @@ DEFAULT_DOCKER_HOST = 'unix:///var/run/docker.sock'
 DEFAULT_SERVICE_PORT = 39999
 
 ULTRADEBUG = '18d1:0304'
+TI50 = '18d1:504a'
 
 
-class GSCDevboardHost(object):
+class GSCDevboardHost(remote.RemoteHost):
     """
     A host that is physically connected to a GSC devboard.
 
@@ -37,6 +54,7 @@ class GSCDevboardHost(object):
                     service_debugger_serial=None,
                     service_ip=None,
                     service_port=DEFAULT_SERVICE_PORT,
+                    service_gsc_serial=None,
                     *args,
                     **dargs):
         """Construct a GSCDevboardHost object.
@@ -49,8 +67,19 @@ class GSCDevboardHost(object):
         @service_port: devboard service port, defaults to 39999.
         """
 
+        super(GSCDevboardHost, self)._initialize(hostname, *args, **dargs)
+
         # Use docker host from environment or by probing a list of candidates.
         self._client = None
+
+        self._docker_container = None
+        self._service_ip = service_ip
+        self._service_port = service_port
+        logging.info("Using service port %s", self._service_port)
+
+        if service_ip is not None:
+            return
+
         try:
             self._client = docker.from_env()
             logging.info("Created docker host from env")
@@ -85,54 +114,105 @@ class GSCDevboardHost(object):
             logging.info("In Satlab")
             self._satlab = True
 
-        self._service_debugger_serial = service_debugger_serial
-        self._service_ip = service_ip
-        self._service_port = service_port
-        logging.info("Using service port %s", self._service_port)
+        image_override = os.path.join(os.path.expanduser('~'),
+                                      SERVICE_IMAGE_OVERRIDE)
+        logging.info('Checking docker image override at %s', image_override)
+        if os.path.exists(image_override):
+            logging.info('Using docker image override')
+            with open(image_override) as f:
+                self._docker_image = f.readline().strip()
+        else:
+            self._docker_image = SERVICE_IMAGE_DEFAULT
+
+        #TODO(b/257333832): Migrate to CFT to manage image on Satlab.
+        token_file = os.path.join(os.path.expanduser('~'),
+                                  SERVICE_IMAGE_TOKEN_FILE)
+        if not self._satlab and os.path.isfile(token_file):
+            try:
+                with open(token_file) as f:
+                    token = f.readline().strip()
+
+                    logging.info('Pulling image %s', self._docker_image)
+                    self._client.login('oauth2accesstoken', token,
+                                       registry='https://gcr.io')
+                    self._client.images.pull(self._docker_image)
+            except docker.errors.NotFound:
+                logging.info('Image not found in registry: %s, assuming local.',
+                             self._docker_image)
+                pass
+            except docker.errors.APIError as e:
+                logging.info('Failed to pull %s: %s, local image may be '
+                             'outdated.', self._docker_image, e)
 
         self._docker_network = 'default_satlab' if self._satlab else 'host'
-        self._docker_container = None
 
-        serials = self._list_debugger_serials()
-        if len(serials) == 0:
-            raise ValueError('No debuggers found')
-        logging.info("Available debuggers: [%s]", ', '.join(serials))
+        self._service_debugger_serial = self._get_valid_serial(ULTRADEBUG,
+            service_debugger_serial)
+        self._service_gsc_serial = self._get_valid_serial(TI50,
+            service_gsc_serial)
 
-        if self._service_debugger_serial is None:
-            self._service_debugger_serial = serials[0]
-        else:
-            if self._service_debugger_serial not in serials:
-                raise ValueError(
-                        '%s debugger not found in [%s]' %
-                        (self._service_debugger_serial, ', '.join(serials)))
+        if (self._service_debugger_serial == "" and
+            self._service_gsc_serial == ""):
+            raise ValueError('No valid debugger nor gsc found')
+
         logging.info("Using debugger %s", self._service_debugger_serial)
+        logging.info("Using gsc %s", self._service_gsc_serial)
+
         self._docker_container_name = "gsc_dev_board_{}".format(
                 self._service_debugger_serial)
 
-    def _list_debugger_serials(self):
-        """List all attached debuggers."""
 
-        c = self._client.containers.run(DOCKER_IMAGE,
+    def _get_valid_serial(self, vidpid, serial):
+        """
+        Gets a valid serial of vidpid that satisfies the provided serial.
+
+        serial is empty string -> device unused(empty string is returned).
+        serial is None -> find unique device(else empty string is returned).
+        serial is given -> must find device(else an error is raised).
+        """
+        if serial == "":
+            return ""
+
+        logging.info("Attempt to find serial given %s", serial)
+
+        serials = self._list_usb_serials(vidpid)
+
+        logging.info("Available %s serials: [%s]", vidpid, ', '.join(serials))
+
+        if serial is None:
+            return serials[0] if len(serials) == 1 else ""
+        if serial in serials:
+            return serial
+        else:
+            raise ValueError('No debuggers found matching %s' % serial)
+
+
+    def _list_usb_serials(self, vidpid):
+        """List all attached devices of vidpid."""
+
+        cmd = ['lsusb', '-v', '-d', vidpid]
+        try:
+            output = self._client.containers.run(self._docker_image,
+                                        cmd,
                                         remove=True,
                                         privileged=True,
-                                        name='list_debugger_serial',
-                                        hostname='list_debugger_serial',
-                                        detach=True,
-                                        volumes=["/dev:/hostdev"],
-                                        command=['sleep', '5'])
+                                        volumes=["/dev:/dev"])
 
-        res, output = c.exec_run(['lsusb', '-v', '-d', ULTRADEBUG],
-                                 stderr=False,
-                                 privileged=True)
-        c.kill()
-        if res != 0:
+            output = output.decode("utf-8").split('\n')
+        except docker.errors.ContainerError:
             return []
-        output = output.decode("utf-8").split('\n')
+
         serials = [
                 l.strip().split(' ')[-1] for l in output
                 if l.strip()[:7] == 'iSerial'
         ]
+
+        if not serials:
+            logging.info('Could not find any serials for %s in: %s', vidpid,
+                         output)
+
         return serials
+
 
     @contextlib.contextmanager
     def service_context(self):
@@ -145,6 +225,7 @@ class GSCDevboardHost(object):
 
     def start_service(self):
         """Starts service if needed."""
+
         if self._docker_container is not None:
             return
 
@@ -153,22 +234,21 @@ class GSCDevboardHost(object):
             logging.info("Skip start_service due to set service_ip")
             return
 
-        #TODO(b/215767105): Pull image onto Satlab box if not present.
-
         environment = {
                 'DEVBOARDSVC_PORT': self._service_port,
-                'DEBUGGER_SERIAL': self._service_debugger_serial
+                'DEBUGGER_SERIAL': self._service_debugger_serial,
+                'GSC_SERIAL': self._service_gsc_serial
         }
         start_cmd = ['/opt/gscdevboard/start_devboardsvc.sh']
 
         # Stop any leftover containers
         try:
             c = self._client.containers.get(self._docker_container_name)
-            c.kill()
-        except docker.errors.NotFound:
+            c.remove(force=True)
+        except docker.errors.APIError:
             pass
 
-        self._client.containers.run(DOCKER_IMAGE,
+        self._docker_container = self._client.containers.run(self._docker_image,
                                     remove=True,
                                     privileged=True,
                                     name=self._docker_container_name,
@@ -176,19 +256,43 @@ class GSCDevboardHost(object):
                                     network=self._docker_network,
                                     cap_add=["NET_ADMIN"],
                                     detach=True,
-                                    volumes=["/dev:/hostdev"],
+                                    volumes=["/dev:/dev"],
                                     environment=environment,
                                     command=start_cmd)
 
-        # A separate containers.get call is needed to capture network attributes
-        self._docker_container = self._client.containers.get(
-                self._docker_container_name)
+        deadline = time.time() + SERVICE_START_TIMEOUT
+        while time.time() <= deadline:
+            try:
+                log = ''
+                self._docker_container.reload()
+                _, log = self._docker_container.exec_run(
+                        ['bash', '-c', 'cat /var/log/devboardsvc_*.log'],
+                        stream=True
+                    )
+                log = '\n'.join(l.decode("utf-8") for l in log)
+                if 'Server started' in log:
+                    logging.info('Using service ip %s', self.service_ip)
+                    return
+                if 'Server failed to start' in log:
+                    break
+            except docker.errors.APIError:
+                break
+
+        logging.debug('Last logs from service: %s', log)
+        self.stop_service()
+        raise RuntimeError('Server failed to start, check if port is already used.')
+
 
     def stop_service(self):
         """Stops service by killing the container."""
         if self._docker_container is None:
             return
-        self._docker_container.kill()
+
+        try:
+            self._docker_container.kill()
+        except docker.errors.NotFound:
+            logging.debug('Service container already stopped.')
+
         self._docker_container = None
 
     @property
--- utils/start_gsc_devboard_host.py
+++ utils/start_gsc_devboard_host.py
@@ -9,14 +9,16 @@ import os
 import common
 from autotest_lib.server.hosts import gsc_devboard_host
 
-# Start service per env vars DOCKER_HOST, DEBUGGER_SERIAL, DEVBOARDSVC_PORT
+# Start service per env vars DOCKER_HOST, DEBUGGER_SERIAL, GSC_SERIAL, DEVBOARDSVC_PORT
 logging.basicConfig(level=logging.INFO)
 e = os.environ
-h = gsc_devboard_host.GSCDevboardHost()
+
+h = gsc_devboard_host.GSCDevboardHost('localhost')
 h._initialize('',
               service_debugger_serial=e.get('DEBUGGER_SERIAL'),
+              service_gsc_serial=e.get('GSC_SERIAL'),
               service_port=e.get('DEVBOARDSVC_PORT',
                                  gsc_devboard_host.DEFAULT_SERVICE_PORT))
 h.start_service()
-logging.info("Service started, container endpoint at %s:%s", h.service_ip,
+logging.info("Service started, gsc service endpoint at %s:%s", h.service_ip,
              h.service_port)

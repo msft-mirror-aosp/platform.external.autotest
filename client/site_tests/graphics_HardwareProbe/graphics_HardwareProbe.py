9fbe99d1b8117a04ab09bd092d01acf44e06c0c7
Shirish S <shirish.s@amd.corp-partner.google.com>
2022-11-18 14:32:18 +0530

    graphics_HardwareProbe: extract gpu name appropriately
    Add "-gpu-family" argument to hardware_probe utility and
    extract the gpu name by splitting its stdout.
    
    BUG=b:259207492
    TEST=graphics_HardwareProbe passes on Skyrim
    
    Signed-off-by: Shirish S <shirish.s@amd.corp-partner.google.com>
    Change-Id: I8f528861c08110fa70009e77a1f5cb7576a2fe41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4035877
    Reviewed-by: Sunil Khatri <sunil.khatri@amd.corp-partner.google.com>
    Reviewed-by: Ilja Friedel <ihf@chromium.org>
    Commit-Queue: Po-Hsien Wang <pwang@chromium.org>
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>

--- client/site_tests/graphics_HardwareProbe/graphics_HardwareProbe.py
+++ client/site_tests/graphics_HardwareProbe/graphics_HardwareProbe.py
@@ -13,7 +13,7 @@ class graphics_HardwareProbe(test.test):
     version = 1
     def run_once(self):
         gpu = utils.get_gpu_family().strip()
-        probe_gpu = utils.run("/usr/local/graphics/hardware_probe").stdout.strip()
+        probe_gpu = utils.run("/usr/local/graphics/hardware_probe -gpu-family").stdout.strip().split('GPU_Family: ')[1]
         if gpu != probe_gpu:
             raise error.TestFail('Failed: autotest %s != hardware_probe %s' % (gpu, probe_gpu))
         logging.info('Got gpu: %s', gpu)

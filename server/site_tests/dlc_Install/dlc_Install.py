e131fe85b3654dac8cf623966289a80c50d418f7
Jae Hoon Kim <kimjae@chromium.org>
2022-12-13 02:48:19 +0000

    Add dlc_Install (scaled) tauto test
    This test will verify on release builds scaled DLC installations.
    
    BUG=b:262320693
    TEST=test_that $IP dlc_Install.scaled
    
    Cq-Depend: chromium:4100064
    Change-Id: I5dd06b0afdbc8dab79d202f4602c80651439c8ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4098619
    Tested-by: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>

---
+++ server/site_tests/dlc_Install/dlc_Install.py
@@ -0,0 +1,32 @@
+# Lint as: python2, python3
+# Copyright 2022 The ChromiumOS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.update_engine import update_engine_test
+
+class dlc_Install(update_engine_test.UpdateEngineTest):
+    """Performs a DLC installation."""
+
+    def initialize(self, host=None, dlc=None):
+        """Remove all DLCs on the DUT before starting the test. """
+        super(dlc_Install, self).initialize(host=host)
+
+        if dlc is None:
+          raise error.TestFail('Must pass in a DLC for testing.')
+
+        self._dlc_util.purge(dlc)
+
+
+    def cleanup(self, dlc):
+        self._dlc_util.purge(dlc)
+
+
+    def run_once(self, dlc):
+        """
+        Install a DLC.
+
+        @param dlc: The name of the DLC to install, DLC ID.
+        """
+        self._dlc_util.install(dlc_id=dlc, omaha_url='')

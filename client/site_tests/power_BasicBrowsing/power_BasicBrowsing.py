182a837b3aa5d48ed8016d4d3850e9cf0912515a
Puthikorn Voravootivat <puthik@google.com>
2023-01-26 15:58:26 +0700

    power: Add power_BasicBrowsing for experiment
    Add new tests for experiment with web content caching.
    This tests is for experiment with the repeatability
    for power test gen2 and will be deleted once Tast
    power library is ready.
    
    BUG=b:266886609
    TEST=Ran the test
    
    Change-Id: I6de30ec4e71f2ea7f10cd167fac6e6291fc92371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4196755
    Tested-by: Puthikorn Voravootivat <puthik@chromium.org>
    Auto-Submit: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-by: Mengqi Guo <mqg@chromium.org>
    Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>

---
+++ client/site_tests/power_BasicBrowsing/power_BasicBrowsing.py
@@ -0,0 +1,85 @@
+# Lint as: python3
+# Copyright 2023 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import logging
+import time
+
+from autotest_lib.client.common_lib.cros import chrome
+from autotest_lib.client.cros.power import power_test
+from autotest_lib.client.cros.power import power_utils
+
+
+class power_BasicBrowsing(power_test.power_Test):
+    """class for power_BasicBrowsing test.
+
+    This tests is used for web content repeatability experiment
+    for power test gen2. This will get deleted once Tast power
+    library is ready.
+    """
+    version = 1
+
+    URL_PREFIX = 'https://storage.googleapis.com/chromiumos-test-assets-public/power_LoadTest/p/'
+    URL_SUFFIX = '.html'
+    SITES = [
+            'google', 'youtube', 'wikipedia', 'yahoo', 'amazon', 'samsung',
+            'naver', 'weather', 'globo', 'aajtak'
+    ]
+
+    def run_once(self,
+                 urls=None,
+                 warmup_secs=20,
+                 secs_per_url=180,
+                 loop_count=2):
+        """run_once method.
+
+        @param urls: list of url to do browsing
+        @param warmup_secs: duration for warmup at the beginning of the test
+        @param secs_per_url: duration to show on each url in each loop
+        @param loop_count: number of loop to test
+        """
+        if urls is None:
+            urls = [
+                    self.URL_PREFIX + site + self.URL_SUFFIX
+                    for site in self.SITES
+            ]
+
+        # --disable-sync disables test account info sync, eg. Wi-Fi credentials,
+        # so that each test run does not remember info from last test run.
+        extra_browser_args = ['--disable-sync']
+        # b/228256145 to avoid powerd restart
+        extra_browser_args.append('--disable-features=FirmwareUpdaterApp')
+        with chrome.Chrome(autotest_ext=True,
+                           extra_browser_args=extra_browser_args,
+                           init_network_controller=True) as self.cr:
+            # Just measure power in full-screen.
+            tab = self.cr.browser.tabs[0]
+            tab.Activate()
+            power_utils.set_fullscreen(self.cr)
+
+            tab.Navigate(urls[0])
+            tab.WaitForDocumentReadyStateToBeComplete()
+            time.sleep(warmup_secs)
+
+            # Stop services and disable multicast again as Chrome might have
+            # restarted them.
+            self._services.stop_services()
+            self._multicast_disabler.disable_network_multicast()
+
+            self.start_measurements()
+
+            task_count = 0
+            for loop in range(loop_count):
+                for i, url in enumerate(urls):
+                    logging.info('Navigating to url: %s', url)
+                    tab.Navigate(url)
+                    tab.WaitForDocumentReadyStateToBeComplete()
+
+                    tagname = '%s_%s' % (self.SITES[i], loop)
+                    self.loop_sleep(task_count, secs_per_url)
+                    self.checkpoint_measurements(tagname, task_count)
+                    task_count += 1
+
+            # Re-enable multicast here instead of in the cleanup because Chrome
+            # might re-enable it and we can't verify that multicast is off.
+            self._multicast_disabler.enable_network_multicast()

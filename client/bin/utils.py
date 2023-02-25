msft-mirror-aosp/platform.external.autotest

commit 596d92fb9b27b0a029418a921b10f71dc80fdd7e

autotest: client: Use rootdev to lookup device for filename
df can enter into an infinite loop if the filename contains a
filesystem.

BUG=b:241137765
TEST=CQ

Change-Id: I372e623bdae6ad32abbe9471d6c1d8367bb13a78
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/3966166
Tested-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Jesse McGuire <jessemcguire@google.com>
Commit-Queue: Sarthak Kukreti <sarthakkukreti@chromium.org>
Reviewed-by: Alexis Savery <asavery@chromium.org>

client/bin/utils.py
@@ -899,7 +899,7 @@ def get_disk_from_filename(filename):
         elif filename.startswith('/dev/loop'):
             cmd = 'losetup -O BACK-FILE "%s" | tail -1' % filename
         else:
-            cmd = 'df "%s" | tail -1 | cut -f 1 -d" "' % filename
+            cmd = 'rootdev -s -d "%s"' % filename
         filename = utils.system_output(cmd)
         m = _DISK_DEV_RE.match(filename)
     return m.group(0)

commit f5b42b8d1b3d8c70867d8818902c2e9a28a09857

autotest: client: Allow rootdev at device lookup to fail
rootdev can be invoked for files who do not have backing devices
(over tmpfs for instance). rootdev supports it but report an error:

rootdev -s -d  /tmp/ramdisk/test ; echo $?
unable to find match
1
rootdev -s -d  /tmp/ramdisk/test 2>/dev/null ; echo $?
1

Allow rootdev to fail, report an empty device.

BUG=b:241137765, b:258894894
TEST=Without fix. `test_that <IP> hardware_RamFio` fails with:
[  FAILED  ]
ERROR: Command <rootdev -s -d "/tmp/ramdisk/test_file"> failed, rc=1, Command returned non-zero exit status
After fix, hardware_RamFio passes.

Fixes: 596d92fb9b2 ("autotest: client: Use rootdev to lookup device for filename")
Change-Id: I970096fd3f20f5ea9e90cd9e370894c61c068fc7
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4277924
Tested-by: Gwendal Grignou <gwendal@chromium.org>
Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>
Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

client/bin/utils.py
@@ -857,7 +857,9 @@ def get_disk_from_filename(filename):
             cmd = 'losetup -O BACK-FILE "%s" | tail -1' % filename
         else:
             cmd = 'rootdev -s -d "%s"' % filename
-        filename = utils.system_output(cmd)
+        filename = utils.system_output(cmd, ignore_status=True)
+        if not filename:
+            return None
         m = _DISK_DEV_RE.match(filename)
     return m.group(0)
 
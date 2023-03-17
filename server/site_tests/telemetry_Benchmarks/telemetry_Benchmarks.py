c4fe0ca2e8e3a5cc1cf1ea070958e76adedc294d
denik <denik@chromium.org>
2023-03-16 17:51:46 -0700

    autotest: Remove "six" workaround
    It was resolve after py3 migration.
    
    BUG=b:206008069
    TEST=bb add benchmark-afdo-generate-orchestrator
    
    Change-Id: I4cdd37ef40712e73b7d618446e5ad2dbf47dd38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4347821
    Commit-Queue: Denis Nikitin <denik@chromium.org>
    Tested-by: Denis Nikitin <denik@chromium.org>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>

--- server/site_tests/telemetry_Benchmarks/telemetry_Benchmarks.py
+++ server/site_tests/telemetry_Benchmarks/telemetry_Benchmarks.py
@@ -4,26 +4,6 @@
 # found in the LICENSE file.
 
 import json
-import logging
-import sys
-
-# TODO (b/206008069), remove this when migrated to new env
-sys.path.insert(0,
-                '/usr/local/lib/python2.7/dist-packages/six-1.16.0-py2.7.egg')
-try:
-    # This is weird. But it seems something is bringing in six earlier
-    # Going to force a reload after the egg is inserted.
-    import six
-    if six.PY2:
-        reload(six)
-    else:
-        import importlib
-        importlib.reload(six)
-    logging.debug("six version is {}".format(six.__version__))
-    if six.__version__ != '1.16.0':
-        logging.debug(sys.path)
-except ImportError as e:
-    logging.warning("Could not import six due to %s", e)
 
 from autotest_lib.server import test
 from autotest_lib.server.cros import telemetry_runner

4b8f21c7af69f21b9e6b4bd3d860dea8e350733f
Derek Basehore <dbasehore@google.com>
2023-03-22 18:07:35 -0700

    power_LW: Add backup restoration of charging
    This adds an additional call to restore charging on the DUT. This will
    help in cases such as when the DUT panics and reboots before re-enabling
    charging. Note that if the DUT shuts down due to low battery, it will
    reboot with charging enabled within a minute.
    
    BUG=b:274828082
    TEST=power_LW.power_LoadTest_1hour
    
    Change-Id: I9ab64a009ba229519b48ccebf7c5bde98b4ea09c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4363113
    Commit-Queue: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>
    Tested-by: Derek Basehore <dbasehore@chromium.org>

--- server/site_tests/power_LW/power_LW.py
+++ server/site_tests/power_LW/power_LW.py
@@ -7,6 +7,7 @@ import logging
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib import utils
+from autotest_lib.client.common_lib.cros import force_discharge_utils
 from autotest_lib.server import autotest
 from autotest_lib.server import test
 from autotest_lib.server.hosts import factory
@@ -111,3 +112,9 @@ class power_LW(test.test):
             autotest_client.run_test(test, **args)
         finally:
             self._start_servo_usb_and_ethernet(host, wlan_host)
+            if not host.wait_up(timeout=30):
+                logging.warning("ethernet connection did not return.")
+                force_discharge_utils.charge_control_by_ectool(True,
+                                                               host=wlan_host)
+            else:
+                force_discharge_utils.charge_control_by_ectool(True, host=host)

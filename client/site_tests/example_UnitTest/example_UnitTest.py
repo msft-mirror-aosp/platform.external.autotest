491b14667639b2e80e27b775bb872692785fec06
Katherine Threlkeld <kathrelkeld@google.com>
2023-04-03 16:29:52 -0700

    [tauto] Delete unclaimed suite/example tests.
    These tests were left unclaimed through the test metadata fixit.
    
    example_UnitTest
    audio_PlaybackRecordSemiAuto
    HWQualAuto
    HWQualBatteryChargeTime
    suite_HWQual.keyboard
    suite_HWQual.power_LoadTest
    suite_HWQual.probe_ac
    suite_HWQual.probe_bat
    suite_TPM.TPMSmogcheck
    
    BUG=b:276788866
    TEST=n/a
    
    Change-Id: Ic95f48765dade39d5698783f480bb94507d70fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4396772
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Tested-by: Katherine Threlkeld <kathrelkeld@chromium.org>
    Commit-Queue: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/site_tests/example_UnitTest/example_UnitTest.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2010 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-from autotest_lib.client.bin import unit_test
-
-class example_UnitTest(unit_test.unit_test):
-    version = 1

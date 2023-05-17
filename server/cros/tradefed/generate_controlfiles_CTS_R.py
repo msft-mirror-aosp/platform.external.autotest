5f8efb773d684a9618f4f06d0ba5826445ecbdd7
Kazuhiro Inaba <kinaba@google.com>
2023-05-17 16:55:22 +0900

    cheets_CTS_R: Even larger camera CTS timelimit
    kukui.kodama takes even more longer time than currently set.
    
    BUG=b:281442281
    TEST=cheets_CTS_T.arm.CtsCameraTestCases
    
    Change-Id: Ic3953d597bc1d2e77a28f24a3cea6c743a7d2709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4543120
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>
    Auto-Submit: Kazuhiro Inaba <kinaba@chromium.org>
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Shao-Chuan Lee <shaochuan@chromium.org>
    Commit-Queue: Shao-Chuan Lee <shaochuan@chromium.org>

--- server/cros/tradefed/generate_controlfiles_CTS_R.py
+++ server/cros/tradefed/generate_controlfiles_CTS_R.py
@@ -116,7 +116,7 @@ CONFIG['CTS_TIMEOUT_DEFAULT'] = 1.0
 CONFIG['CTS_TIMEOUT'] = {
         'CtsAppSecurityHostTestCases': 2.0,
         'CtsAutoFillServiceTestCases': 2.5,  # TODO(b/134662826)
-        'CtsCameraTestCases': 2.0,
+        'CtsCameraTestCases': 2.5,
         'CtsDeqpTestCases': 30.0,
         'CtsDeqpTestCases.dEQP-EGL': 2.0,
         'CtsDeqpTestCases.dEQP-GLES2': 2.0,

cf7977d7789deab3f6684c3d19bd52a2e166d512
Mary Ruthven <mruthven@chromium.org>
2023-08-11 10:01:13 -0700

    cr50_test: fix can update image check
    BUG=b:295536295
    TEST=run firmware_GSCPinweaverUpdate on zork
    
    Change-Id: I4a2bb87a330332f6a91836f4d6d59997a16e3271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4775144
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Edward Hill <ecgh@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>

--- server/cros/faft/cr50_test.py
+++ server/cros/faft/cr50_test.py
@@ -65,17 +65,19 @@ class Cr50Test(FirmwareTest):
 
         if ((restore_cr50_image or restore_cr50_board_id)
                     and self.servo.main_device_uses_gsc_drv()
-                    and self.gsc.running_mp_image()
-                    and not self.servo.has_control('gsc_ecrst_pulse')
-                    and not self.servo.has_control('cold_reset_select')):
-            # Tests that restore the image or the board id have to update
-            # to the DBG image. This clears testlab mode. If a board relies
-            # on ccd for basic servo functionality, this could make the dut
-            # unusable. Skip the tests. Rely on other boards and prepvt
-            # images to cover the test.
-            raise error.TestNAError(
-                    'Test clears testlab mode. Cannot '
-                    'run with mp images on boards that use ccd for servo')
+                    and self.gsc.running_mp_image()):
+            if (self.servo.has_control('gsc_ecrst_pulse')
+                        and self.servo.has_control('cold_reset_select')):
+                logging.info('Can use ecrst pulse to reenable testlab mode')
+            else:
+                # Tests that restore the image or the board id have to update
+                # to the DBG image. This clears testlab mode. If a board relies
+                # on ccd for basic servo functionality, this could make the dut
+                # unusable. Skip the tests. Rely on other boards and prepvt
+                # images to cover the test.
+                raise error.TestNAError(
+                        'Test clears testlab mode. Cannot '
+                        'run with mp images on boards that use ccd for servo')
 
         logging.info('Test Args: %r', full_args)
 

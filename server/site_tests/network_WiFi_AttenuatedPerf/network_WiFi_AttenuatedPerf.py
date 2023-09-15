64acea59cf6f0e540bf4aa215cbad774d8114435
Jason Stanko <jstanko@google.com>
2023-09-15 21:46:33 +0000

    Reland "wifi: Automatically calculate and report knee-point"
    This relands RvR knee-point calculation. Moved to using numpy.arange
    rather than normal range function to avoid type errors caused by
    argument difference when running via CFT.
    
    This also wraps the test execution in a try/finally to avoid leaving
    perf_monitor open on the event of a failure which was responsible for
    the job not terminating when running via CFT.
    
    BUG=b:291746718
    TEST=run attenuated perf via CFT
    TEST=network_WiFi_AttenuatedPerf.ht40_ch001
    
    This is a reland of commit 8e732bb2de2630f84b36557bbdb291a10cd1561d
    
    Original change's description:
    > wifi: Automatically calculate and report knee-point
    >
    > This automatically calculates and reports the RvR knee points to
    > crosbolt. It includes a secondary run of throughput measurements at a
    > finer resolution around the knee point to ensure that we're accurately
    > resolving it. The individual refinement data points are not reported to
    > crosbolt but the improved final knee-point estimation is.
    >
    > BUG=b:291746718
    > TEST=test_that --args="router_addr=localhost:2201 \
    >   pcap_addr=localhost:2202 atten_addr=localhost:2203" \
    >   localhost:2200 network_WiFi_AttenuatedPerf.ht40_ch001
    >
    > Change-Id: Ia97448459bf9faa0aa9c5df797bb4ac638114e0f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4867884
    > Reviewed-by: Arowa Suliman <arowa@chromium.org>
    > Reviewed-by: Kevin Lund <kglund@google.com>
    > Tested-by: Jason Stanko <jstanko@google.com>
    > Commit-Queue: Jason Stanko <jstanko@google.com>
    
    Bug: b:291746718
    Change-Id: I9dc63af9e2b5316bab18b9e73efe46495f566b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5203278
    Reviewed-by: Kevin Lund <kglund@google.com>
    Tested-by: Jason Stanko <jstanko@google.com>
    Commit-Queue: Jason Stanko <jstanko@google.com>

--- server/site_tests/network_WiFi_AttenuatedPerf/network_WiFi_AttenuatedPerf.py
+++ server/site_tests/network_WiFi_AttenuatedPerf/network_WiFi_AttenuatedPerf.py
@@ -7,6 +7,7 @@ import collections
 import logging
 import os.path
 import time
+import numpy
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib.cros.network import xmlrpc_datatypes
@@ -14,6 +15,10 @@ from autotest_lib.server.cros.network import netperf_runner
 from autotest_lib.server.cros.network import netperf_session
 from autotest_lib.server.cros.network import wifi_cell_test_base
 from autotest_lib.server.cros.network import perf_monitor_service
+
+
+def _interpolate(tput, atten1, atten2, tput1, tput2):
+    return atten1 + ((tput - tput1) * (atten2 - atten1)) / (tput2 - tput1)
 
 
 class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
@@ -60,11 +65,21 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
         if self.CMDLINE_SERIES_NOTE in commandline_args:
             self.series_note = commandline_args[self.CMDLINE_SERIES_NOTE]
 
-
     def run_once(self):
+        try:
+            # Start the performance monitoring of network throughput.
+            perf_monitor = perf_monitor_service.PerfMonitorService(
+                    self.context.client.host)
+            perf_monitor.start_monitoring_throughput()
+            self._run_once()
+
+        finally:
+            # Stop the performance monitoring of network throughput.
+            perf_monitor.stop_monitoring_throughput()
+
+    def _run_once(self):
         """Run test."""
         start_time = time.time()
-        throughput_data = []
         max_atten = None
         self.context.client.host.get_file('/etc/lsb-release', self.resultsdir)
         # Set up the router and associate the client with it.
@@ -74,9 +89,6 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
                 security_config=self._ap_config.security_config)
         self.context.assert_connect_wifi(assoc_params)
 
-        # Start the performance monitoring of network throughput.
-        perf_monitor = perf_monitor_service.PerfMonitorService(self.context.client.host)
-        perf_monitor.start_monitoring_throughput()
 
         # Conduct the performance tests.  Ignore failures, since
         # at high attenuations, sometimes the control connection
@@ -86,8 +98,155 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
                                                  ignore_failures=True)
         session.warmup_stations()
         start_atten = self.context.attenuator.get_minimal_total_attenuation()
-        for atten in range(start_atten, self._final_attenuation,
-                           self._attenuation_increment):
+
+        throughput_data = self._run_sweep(session, self.NETPERF_CONFIGS,
+                                          start_atten, self._final_attenuation,
+                                          self._attenuation_increment, True)
+
+        self._refine_knee_point(session, throughput_data)
+
+        # Clean up router and client state.
+        self.context.client.shill.disconnect(assoc_params.ssid)
+        self.context.router.deconfig()
+        end_time = time.time()
+        logging.info('Running time %0.1f seconds.', end_time - start_time)
+
+        max_atten = throughput_data[-1] if throughput_data else None
+        if max_atten is None:
+            raise error.TestFail('Did not succeed at any atten level')
+
+        logging.info('Reached attenuation of: %d dB (signal %d)', max_atten[0],
+                     max_atten[1])
+        self.write_perf_keyval(
+                {'ch%03d_max_atten' % self._ap_config.channel: max_atten[0]})
+        self.write_perf_keyval(
+                {'ch%03d_min_signal' % self._ap_config.channel: max_atten[1]})
+
+        self._report_knee_results(throughput_data)
+        self.write_throughput_tsv_files(throughput_data)
+
+    def _report_knee_results(self, throughput_data):
+        """Reports knee point results to crosbolt.
+
+        @param throughput_data a list of Datapoint namedtuples gathered from
+                tests.
+        """
+        for test_type in set([data.test_type for data in throughput_data]):
+            # TODO(b/303452801): Once knee point calculation is verified as stable,
+            # raise error after reporting if calculation fails for any configs
+            knee_atten, knee_throughput = self._calculate_knee_point(
+                    throughput_data, test_type)
+            if not knee_atten:
+                continue
+            logging.info(
+                    'Calculated knee point attenuation: %f, throughput %f',
+                    knee_atten, knee_throughput)
+
+            # Write to crosbolt.
+            graph_name = '.'.join(
+                    [self._ap_config.perf_loggable_description, test_type])
+            self.output_perf_value("knee_atten",
+                                   knee_atten,
+                                   units='dBm',
+                                   higher_is_better=True,
+                                   graph=graph_name)
+            self.output_perf_value("knee_throughput",
+                                   knee_throughput,
+                                   units='Mbps',
+                                   higher_is_better=True,
+                                   graph=graph_name)
+
+            # Log knee point results.
+            tag = '%s_%s' % (self._ap_config.perf_loggable_description,
+                             test_type)
+            self.write_perf_keyval({'%s_knee_atten' % tag: knee_atten})
+            self.write_perf_keyval(
+                    {'%s_knee_throughput' % tag: knee_throughput})
+
+    def _refine_knee_point(self, session, throughput_data):
+        """Runs additional refinement iterations around knee-points.
+
+        Refinement results are not reported to Crosbolt, but are appended to
+        existing results and included in the output *.tsv files.
+
+        @param session the perf session
+        @param throughput_data a list of Datapoint namedtuples gathered from
+                tests.
+        """
+        # Calculate knee_point and re-run calculation for each config.
+        for config in self.NETPERF_CONFIGS:
+            knee_atten, knee_throughput = self._calculate_knee_point(
+                    throughput_data, config.tag)
+            if knee_atten is None:
+                continue
+
+            logging.info(
+                    'Calculated knee point attenuation: %f, throughput %f',
+                    knee_atten, knee_throughput)
+
+            # Run knee point at double resolution around calculated value.
+            used_attens = set([dp.attenuation for dp in throughput_data])
+            # Midpoint round to keep results somewhat consistent.
+            knee_atten = round(knee_atten)
+            step_size = max(1, self._attenuation_increment // 2)
+            for atten in numpy.arange(knee_atten - step_size,
+                                      knee_atten + step_size + 1, step_size):
+                if atten in used_attens:
+                    continue
+                # Just run a single point.
+                new_points = self._run_sweep(session, [config], atten,
+                                             atten + 1, 1, False)
+                throughput_data.extend(new_points)
+                throughput_data.sort(key=lambda d: d.attenuation)
+
+    def _calculate_knee_point(self, throughput_data, test_type):
+        """Performs a simple linear interpolation to estimate knee point.
+
+        Knee point is estimated to be at 90% of maximum throughput. This is not
+        guaranteed to be unique since we're interpolating on the y value,
+        however, results are generally well-behaved before the knee point and do
+        not exhibit much noise in this region.
+
+        @param throughput_data a list of Datapoint namedtuples gathered from
+                tests.
+        @return test_type the test_type of the Datapoints to filter on.
+        """
+        throughput_data = [
+                dp for dp in throughput_data if dp.test_type == test_type
+        ]
+        max_throughput = max(throughput_data, key=lambda d: d.throughput)
+        find = 0.9 * max_throughput.throughput
+        logging.info('Looking for knee point at target throughput %f', find)
+
+        for i in range(len(throughput_data) - 1):
+            dp1 = throughput_data[i]
+            dp2 = throughput_data[i + 1]
+            if dp1.throughput > find and dp2.throughput < find:
+                atten = _interpolate(find, dp1.attenuation, dp2.attenuation,
+                                     dp1.throughput, dp2.throughput)
+                return (atten, find)
+
+            # Corner cases if knee point happens to fall exactly on a sample.
+            if dp1.throughput == find:
+                return (dp1.attenuation, dp1.throughput)
+            if dp2.throughput == find:
+                return (dp2.attenuation, dp2.throughput)
+
+        logging.error('Failed to find knee_point')
+        return (None, None)
+
+    def _run_sweep(self, session, configs, start, end, step, report_results):
+        """Sweeps through set of attenuations and configs and returns results.
+
+        @param session the perf session
+        @param configs the set of configurations to run
+        @param start the starting attenuation
+        @param end the final attenuation
+        @param step the attenuation step size
+        @param report_results true if the results should be reported to crosbolt
+        """
+        throughput_data = []
+        for atten in numpy.arange(start, end, step):
             atten_tag = 'atten%03d' % atten
             self.context.attenuator.set_total_attenuation(
                     atten, self._ap_config.frequency)
@@ -103,7 +262,7 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
                                 atten, str(e))
                 break
 
-            for config in self.NETPERF_CONFIGS:
+            for config in configs:
                 results = session.run(config)
                 if not results:
                     logging.warning('Unable to take measurement for %s; '
@@ -116,12 +275,19 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
                 # signal_level to -100 to indicate weak signal.
                 signal_level = (self.context.client.wifi_signal_level if
                         self.context.client.wifi_signal_level else -100)
-                self.output_perf_value(atten_tag, values, units='Mbps',
-                                       higher_is_better=True, graph=graph_name)
-                self.output_perf_value('_'.join([atten_tag, 'signal']),
-                                       signal_level,
-                                       units='dBm', higher_is_better=True,
-                                       graph=graph_name)
+
+                if report_results:
+                    self.output_perf_value(atten_tag,
+                                           values,
+                                           units='Mbps',
+                                           higher_is_better=True,
+                                           graph=graph_name)
+                    self.output_perf_value('_'.join([atten_tag, 'signal']),
+                                           signal_level,
+                                           units='dBm',
+                                           higher_is_better=True,
+                                           graph=graph_name)
+
                 result = netperf_runner.NetperfResult.from_samples(results)
                 throughput_data.append(self.DataPoint(
                         atten,
@@ -129,12 +295,12 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
                         result.throughput_dev,
                         signal_level,
                         config.tag))
-                keyval_prefix = '_'.join(
-                        [self._ap_config.perf_loggable_description, config.tag,
-                         atten_tag])
+
+                keyval_prefix = '_'.join([
+                        self._ap_config.perf_loggable_description, config.tag,
+                        atten_tag
+                ])
                 self.write_perf_keyval(result.get_keyval(prefix=keyval_prefix))
-                # Reported at least one successful result at this attenuation.
-                max_atten = (atten, signal_level)
 
             signal_level = self.context.client.wifi_signal_level
             self.write_perf_keyval(
@@ -144,25 +310,7 @@ class network_WiFi_AttenuatedPerf(wifi_cell_test_base.WiFiCellTestBase):
                 logging.warning('No results for atten %d dB; terminating',
                                 atten)
 
-        # Stop the performance monitoring of network throughput.
-        perf_monitor.stop_monitoring_throughput()
-
-        # Clean up router and client state.
-        self.context.client.shill.disconnect(assoc_params.ssid)
-        self.context.router.deconfig()
-        end_time = time.time()
-        logging.info('Running time %0.1f seconds.', end_time - start_time)
-
-        if max_atten is None:
-            raise error.TestFail('Did not succeed at any atten level')
-        logging.info('Reached attenuation of: %d dB (signal %d)',
-                                max_atten[0], max_atten[1])
-        self.write_perf_keyval({'ch%03d_max_atten' % self._ap_config.channel:
-                                max_atten[0]})
-        self.write_perf_keyval({'ch%03d_min_signal' % self._ap_config.channel:
-                                max_atten[1]})
-        self.write_throughput_tsv_files(throughput_data)
-
+        return throughput_data
 
     def write_throughput_tsv_files(self, throughput_data):
         """Write out .tsv files with plotable data from |throughput_data|.

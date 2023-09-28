cd71bc15faa2cc4540852b7de65432c887443d2e
Nehemiah Dureus <ndureus@chromium.org>
2023-09-28 00:20:37 +0000

    pvs: Remove suites and requirements and add TODO
    We no longer need the firmware_ECSharedMem, as it has been migrated
    to TSAT, but it is used by PVS and can't be deleted yet.
    
    BUG=b:194908259, b:290823172
    TEST=None
    
    Change-Id: I50673fed88eb23c517a7cfb482889a4e46386d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/4898520
    Commit-Queue: Nehemiah Dureus <ndureus@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Nehemiah Dureus <ndureus@chromium.org>

--- server/site_tests/firmware_ECSharedMem/firmware_ECSharedMem.py
+++ server/site_tests/firmware_ECSharedMem/firmware_ECSharedMem.py
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# TODO(b/290823172) Remove this control file when the test is no longer in any
+# PVS plan
+
 import logging
 import time
 

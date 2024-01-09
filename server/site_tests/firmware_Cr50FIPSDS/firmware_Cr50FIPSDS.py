f3e41649885090ecdc2056b394fe7e222bb234ab
Mary Ruthven <mruthven@chromium.org>
2024-01-09 08:53:55 -0800

    Cr50FIPSDS: minor test fixes
    Use poweroff to shutdown the device. It's what we use in the device
    state tests. We know that works.
    
    Check the EC console at the start of the test to verify it works ok.
    
    Disable on ccd setups. The test verifies behavior through deep sleep.
    This is tricky without servo micro and c2d2. Disable the test on ccd
    setups.
    
    Add more logging.
    
    BUG=b:319253418
    TEST=run on volteer
    
    Change-Id: I25fb0de7c6ca8516fccd2b10bfe138cb4114441a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5180412
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Aadit Maheshkumar Modi <modia@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>

--- server/site_tests/firmware_Cr50FIPSDS/firmware_Cr50FIPSDS.py
+++ server/site_tests/firmware_Cr50FIPSDS/firmware_Cr50FIPSDS.py
@@ -6,7 +6,6 @@ import logging
 import time
 
 from autotest_lib.client.common_lib import error
-from autotest_lib.client.common_lib.cros import cr50_utils
 from autotest_lib.server.cros.faft.cr50_test import Cr50Test
 
 
@@ -18,8 +17,9 @@ class firmware_Cr50FIPSDS(Cr50Test):
 
     def apshutdown(self):
         """Shutdown the AP and give cr50 enough time to enter deep sleep."""
+        self._try_to_bring_dut_up()
         self.gsc.ccd_disable()
-        self.set_ap_off_power_mode('shutdown')
+        self.faft_client.system.run_shell_command('poweroff', True)
         self.gsc.clear_deep_sleep_count()
         time.sleep(30)
 
@@ -34,14 +34,23 @@ class firmware_Cr50FIPSDS(Cr50Test):
         if not self.gsc.get_deep_sleep_count():
             raise error.TestError('Cr50 did not enter deep sleep')
         # Make sure the DUT fully booted and is sshable.
-        logging.info('Running %r', cr50_utils.GetRunningVersion(self.host))
+        logging.info('Running %r', self.gsc.get_version())
+        logging.info('AP State %r', self.try_to_get_ap_state())
 
     def run_once(self, host):
         """Verify FIPS after deep sleep."""
         if not self.check_ec_capability(suppress_warning=True):
             raise error.TestNAError('Only supported on devices with ECs')
+        if self.servo.main_device_is_ccd():
+            raise error.TestNAError('Only supported with servo flex cable')
         if not self.gsc.has_command('fips'):
             raise error.TestNAError('Cr50 does not support fips')
+        # Verify the EC console works.
+        self.servo.enable_main_servo_device()
+        try:
+            self.ec.get_version()
+        except Exception as e:
+            raise error.TestError('EC console unresponsive: %s' % e)
 
         # Verify EC sysjump works on deep sleep resume.
         self.apshutdown()

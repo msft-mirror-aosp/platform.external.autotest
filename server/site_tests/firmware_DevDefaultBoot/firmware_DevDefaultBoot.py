8661465c71f6d2cd5329701ead4bb8743b0acc9b
Jeremy Bettis <jbettis@google.com>
2024-01-29 14:27:12 -0700

    Remove suites, add TODO to firmware_DevDefaultBoot
    BUG=b:270207704,b:322848860
    TEST=None
    
    Cq-Depend: chromium:5241023
    Change-Id: Ibd6afc6000485f37122084dbb570d3f9f0ae1da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5246264
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Kshitij Shah <tij@google.com>

--- server/site_tests/firmware_DevDefaultBoot/firmware_DevDefaultBoot.py
+++ server/site_tests/firmware_DevDefaultBoot/firmware_DevDefaultBoot.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# TODO(b/322848860): Remove from PVS testplans and remove this file.
+
 import logging
 
 from autotest_lib.client.common_lib import error

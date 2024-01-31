239cfbafc2302c4b4eb35a147e545d2ad28df153
Jae Hoon Kim <kimjae@google.com>
2024-01-31 01:14:40 +0000

    autotest: Add cache server constants
    .. for the "forever" load balancing IPs.
    
    BUG=none
    TEST=CQ
    
    Change-Id: I6b0e722855d65882765161bff3afa265135f1442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5252111
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Tested-by: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Congbin Guo <guocb@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>

---
+++ cache_server/constants.py
@@ -0,0 +1,8 @@
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""This module contains cache server related constants."""
+
+CHROMEOS_6_CACHE = "100.115.168.190"
+CHROMEOS_8_CACHE = "10.128.176.210"
+CHROMEOS_15_CACHE = "100.115.21.212"

69e0513453e97954bdca3a3a2e11290dd75d7e13
Daniil Lunev <dlunev@google.com>
2024-02-22 10:46:47 +1100

    storage: remove unnecessary batches
    BUG=none
    TEST=none
    
    Change-Id: I51f2a10f52a847eeb8bf0a7079e9a0303468c81f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5315137
    Tested-by: Daniil Lunev <dlunev@chromium.org>
    Commit-Queue: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Alexis Savery <asavery@chromium.org>

--- server/site_tests/hardware_StorageQualV3/generate_control_files.py
+++ server/site_tests/hardware_StorageQualV3/generate_control_files.py
@@ -26,90 +26,77 @@ def _read_template_file(filename):
 
 
 v3_tests = [
-        TestEntry(name='storage_testing_v3_basic_info',
-                  test_expr=[
-                          'storage.InternalDeviceTypeCheck',
-                          'storage.HealthInfo', 'storage.FfuSupport'
-                  ],
-                  attributes=[
-                          'pdp-enabled', 'pdp-kpi', 'pdp-stress', 'avl-v3',
-                          'avl-v3-batch1'
-                  ],
-                  duration=1200),
-        TestEntry(name='storage_testing_v3_device_config',
-                  test_expr=[
-                          'storage.EmmcConfiguration',
-                          'storage.UfsConfiguration',
-                          'storage.NvmeConfiguration'
-                  ],
-                  attributes=[
-                          'pdp-enabled', 'pdp-kpi', 'pdp-stress', 'avl-v3',
-                          'avl-v3-batch1'
-                  ],
-                  duration=1200),
-        TestEntry(name='storage_testing_v3_erase_behaviour',
-                  test_expr=[
-                          'storage.Trim', 'storage.WriteZeroCorrectness',
-                          'storage.WriteZeroPerf'
-                  ],
-                  attributes=[
-                          'pdp-enabled', 'pdp-kpi', 'pdp-stress', 'avl-v3',
-                          'avl-v3-batch1'
-                  ],
-                  duration=12000),
+        TestEntry(
+                name='storage_testing_v3_basic_info',
+                test_expr=[
+                        'storage.InternalDeviceTypeCheck',
+                        'storage.HealthInfo', 'storage.FfuSupport'
+                ],
+                attributes=['pdp-enabled', 'pdp-kpi', 'pdp-stress', 'avl-v3'],
+                duration=1200),
+        TestEntry(
+                name='storage_testing_v3_device_config',
+                test_expr=[
+                        'storage.EmmcConfiguration',
+                        'storage.UfsConfiguration', 'storage.NvmeConfiguration'
+                ],
+                attributes=['pdp-enabled', 'pdp-kpi', 'pdp-stress', 'avl-v3'],
+                duration=1200),
+        TestEntry(
+                name='storage_testing_v3_erase_behaviour',
+                test_expr=[
+                        'storage.Trim', 'storage.WriteZeroCorrectness',
+                        'storage.WriteZeroPerf'
+                ],
+                attributes=['pdp-enabled', 'pdp-kpi', 'pdp-stress', 'avl-v3'],
+                duration=12000),
         TestEntry(name='storage_testing_v3_soc_perf',
                   test_expr=['storage.SocPerformance.*'],
                   attributes=['pdp-kpi', 'pdp-stress'],
                   duration=1200),
         TestEntry(name='storage_testing_v3_power_state',
                   test_expr=['storage.LowPowerStateResidence'],
-                  attributes=[
-                          'pdp-kpi', 'pdp-stress', 'avl-v3', 'avl-v3-batch1'
-                  ],
+                  attributes=['pdp-kpi', 'pdp-stress', 'avl-v3'],
                   duration=600),
         TestEntry(name='storage_testing_v3_part_perf',
                   test_expr=['storage.Performance.*'],
-                  attributes=['avl-v3', 'avl-v3-batch1'],
+                  attributes=['avl-v3'],
                   duration=7800),
         TestEntry(name='storage_testing_v3_stress_write',
                   test_expr=['storage.StressWrite'],
-                  attributes=['pdp-stress', 'avl-v3', 'avl-v3-batch1'],
+                  attributes=['pdp-stress', 'avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress_write_2',
                   test_expr=[
                           'storage.StressWrite.iteration_2',
                   ],
-                  attributes=['avl-v3', 'avl-v3-batch2'],
+                  attributes=['avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress_write_3',
-                  test_expr=[
-                          'storage.StressWrite.iteration_3'
-                  ],
-                  attributes=['avl-v3', 'avl-v3-batch2'],
+                  test_expr=['storage.StressWrite.iteration_3'],
+                  attributes=['avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress_suspend',
                   test_expr=['storage.SuspendStress'],
-                  attributes=['pdp-stress', 'avl-v3', 'avl-v3-batch1'],
+                  attributes=['pdp-stress', 'avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress_suspend_2',
                   test_expr=[
                           'storage.SuspendStress.iteration_2',
                   ],
-                  attributes=['avl-v3', 'avl-v3-batch3'],
+                  attributes=['avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress_suspend_3',
-                  test_expr=[
-                          'storage.SuspendStress.iteration_3'
-                  ],
-                  attributes=['avl-v3', 'avl-v3-batch3'],
+                  test_expr=['storage.SuspendStress.iteration_3'],
+                  attributes=['avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress',
                   test_expr=['storage.Stress'],
-                  attributes=['pdp-stress', 'avl-v3', 'avl-v3-batch1'],
+                  attributes=['pdp-stress', 'avl-v3'],
                   duration=28800),
         TestEntry(name='storage_testing_v3_stress_2',
                   test_expr=['storage.Stress.iteration_2'],
-                  attributes=['avl-v3', 'avl-v3-batch4'],
+                  attributes=['avl-v3'],
                   duration=28800),
 ]
 

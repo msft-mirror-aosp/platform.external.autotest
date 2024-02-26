75d3d05c2576f90d40f3defc4dec1ececcb074db
Mary Ruthven <mruthven@chromium.org>
2024-02-26 17:38:05 -0600

    provision_Cr50TOT: add ti50 support
    Add support for downloading ti50 tot images from the postsubmit builder.
    
    BUG=b:277805456
    TEST=run on faft-cr50-tot nissa
    
    Change-Id: Ic17880fbff201849dd219ae3ee0ab8d745c5f44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5324111
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Aadit Maheshkumar Modi <modia@chromium.org>
    Auto-Submit: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- server/site_tests/provision_Cr50TOT/provision_Cr50TOT.py
+++ server/site_tests/provision_Cr50TOT/provision_Cr50TOT.py
@@ -18,14 +18,25 @@ from autotest_lib.server.cros.faft.cr50_test import Cr50Test
 
 
 # TOT cr50 images are built as part of the reef image builder.
-BUILDER = 'reef'
-GS_URL = 'gs://chromeos-releases/dev-channel/' + BUILDER
+CR50_BUILDER = 'reef'
+CR50_GS_URL = 'gs://chromeos-releases/dev-channel/' + CR50_BUILDER
 # Firmware artifacts are stored in files like this.
 #   ChromeOS-firmware-R79-12519.0.0-reef.tar.bz2
-FIRMWARE_NAME = 'ChromeOS-firmware-%s-%s.tar.bz2'
-REMOTE_TMPDIR = '/tmp/cr50_tot_update'
+CR50_FIRMWARE_TARBALL = 'ChromeOS-firmware-%s-%s.tar.bz2'
 CR50_IMAGE_PATH = 'cr50/ec.bin'
+
+# Ti50 file info
+TI50_GS_URL = 'gs://chromeos-releases/firmware-ti50-postsubmit/%s-*/ti50.tar.bz2'
+TI50_FILENAME = 'ti50_Unknown_PrePVT_ti50-accessory-nodelocked-ro-premp.bin'
+
+REMOTE_TMPDIR = '/tmp/gsc_tot_update'
 VER_RE = r'R(\d*)-(\d*).(\d*).(\d*)'
+
+
+def get_gs_paths(path):
+    """Return a list of gs paths."""
+    cmd = 'gsutil ls -- %s' % path
+    return utils.system_output(cmd).splitlines()
 
 # Wait 10 seconds for the update to take effect.
 class provision_Cr50TOT(Cr50Test):
@@ -52,9 +63,8 @@ class provision_Cr50TOT(Cr50Test):
               contents of the path gs://<bucket>/<board> for any reason.
         """
         path = 'gs://%s/%s' % (bucket, board)
-        cmd = 'gsutil ls -- %s' % path
         try:
-            contents = utils.system_output(cmd).splitlines()
+            contents = get_gs_paths(path)
             builds = []
             for content in contents:
                 m = re.search(VER_RE, content)
@@ -73,10 +83,25 @@ class provision_Cr50TOT(Cr50Test):
             raise error.TestFail('Could not determine the latest build due '
                                  'to exception: %s' % e)
 
+    def get_ti50_build(self, latest_ver, remote_dir):
+        """Download the TOT ti50 image from the postsubmit builder."""
+        path = get_gs_paths(
+                os.path.join(TI50_GS_URL % latest_ver, TI50_FILENAME))[0]
+        logging.info('Using ti50 image from %s', path)
+        bucket = os.path.dirname(path)
+        filename = os.path.basename(path)
+        # Download the firmware artifacts from google storage.
+        gsutil_wrapper.copy_private_bucket(host=self.host,
+                                           bucket=bucket,
+                                           filename=filename,
+                                           destination=remote_dir)
+        return os.path.join(remote_dir, filename)
+
+
     def get_cr50_build(self, latest_ver, remote_dir):
         """Download the TOT cr50 image from the reef artifacts."""
-        bucket = os.path.join(GS_URL, latest_ver.split('-')[-1])
-        filename = FIRMWARE_NAME % (latest_ver, BUILDER)
+        bucket = os.path.join(CR50_GS_URL, latest_ver.split('-')[-1])
+        filename = CR50_FIRMWARE_TARBALL % (latest_ver, CR50_BUILDER)
         logging.info('Using cr50 image from %s', latest_ver)
 
         # Download the firmware artifacts from google storage.
@@ -91,39 +116,42 @@ class provision_Cr50TOT(Cr50Test):
         return os.path.join(remote_dir, CR50_IMAGE_PATH)
 
 
-    def get_latest_cr50_build(self):
+    def get_latest_gsc_build(self):
         self.host.run('mkdir -p %s' % (REMOTE_TMPDIR))
         latest_builds = self.get_latest_builds()
         for latest_build in latest_builds:
             try:
+                if self.gsc.NAME == 'ti50':
+                    return self.get_ti50_build(latest_build, REMOTE_TMPDIR)
                 return self.get_cr50_build(latest_build, REMOTE_TMPDIR)
             except Exception as e:
-                logging.warning('Unable to find %s cr50 image %s',
-                                latest_build, e)
-        raise error.TestFail('Unable to find latest cr50 image in %s' %
+                logging.warning('Unable to find %s gsc image %s', latest_build,
+                                e)
+        raise error.TestFail('Unable to find latest gsc image in %s' %
                              latest_builds)
 
 
     def run_once(self, host, force=False):
-        """Update cr50 to the TOT image from the reef builder."""
+        """Update GSC to a recent TOT image."""
         # TODO(mruthven): remove once the test is successfully scheduled.
         logging.info('SUCCESSFULLY SCHEDULED PROVISION CR50 TOT UPDATE')
         if not force:
             logging.info('skipping update')
             return
         self._provision_update = True
-        logging.info('cr50 version %s', host.servo.get('cr50_version'))
+        logging.info('%s version %s', self.gsc.NAME,
+                     host.servo.get('cr50_version'))
         self.host = host
-        cr50_path = self.get_latest_cr50_build()
-        logging.info('cr50 image is at %s', cr50_path)
-        local_path = os.path.join(self.resultsdir, 'cr50.bin.tot')
-        self.host.get_file(cr50_path, local_path)
+        gsc_path = self.get_latest_gsc_build()
+        logging.info('%s image is at %s', self.gsc.NAME, gsc_path)
+        local_path = os.path.join(self.resultsdir, 'gsc.bin.tot')
+        self.host.get_file(gsc_path, local_path)
 
-        cr50_utils.GSCTool(self.host, ['-a', cr50_path])
+        cr50_utils.GSCTool(self.host, ['-a', gsc_path])
 
         self.gsc.wait_for_reboot(
                 timeout=self.faft_config.gsc_update_wait_for_reboot)
-        cr50_version = self.gsc.get_active_version_info()[3].split('/')[-1]
-        logging.info('Cr50 running %s after update', cr50_version)
+        gsc_version = self.gsc.get_active_version_info()[3].split('/')[-1]
+        logging.info('%s running %s after update', self.gsc.NAME, gsc_version)
         self.make_rootfs_writable()
         cr50_utils.InstallImage(self.host, local_path, self.gsc.DUT_PREPVT)

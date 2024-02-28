1ac247992172940fa51ea328ed361c454084a247
Mary Ruthven <mruthven@chromium.org>
2024-02-28 12:12:20 -0600

    cr50_test: corrupt the inactive image instead of reflashing it
    Corrupt the inactive image instead of flashing both regions after
    rollback. This will save over a minute since release images wait a
    minute before accepting an update. Corrupting the DBG image can happen
    immediately.
    
    BUG=b:327422501
    TEST=run provision_Cr50Update, firmware_Cr50Update, and
    firmware_GSCPinweaverUpdate on ti50 and cr50
    
    Change-Id: Iccb66cef6d43eb68454c4b27058b8d0a7f8bd32f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5333277
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Jett Rink <jettrink@google.com>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- client/common_lib/cros/cr50_utils.py
+++ client/common_lib/cros/cr50_utils.py
@@ -103,6 +103,8 @@ gsctool.add_argument('-o', '--ccd_open', dest='ccd_open', action='store_true')
 # something went wrong.
 gsctool.add_argument('-b',
                      '--binvers',
+                     '-c',
+                     '--corrupt',
                      '-f',
                      '--fwver',
                      '-g',

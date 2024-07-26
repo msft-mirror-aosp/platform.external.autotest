7c4704a5178f743ad5f4a6299bdcce2fa434318d
Peggy Chen <peggy.chen@cienet.com>
2024-07-26 08:40:56 +0800

    autotest: Add TODO to remove firmware_UpdateKernelSubkeyVersion
    The test has been migrated to the subtest kernel_subkey_version in 
    firmware.UpdateVersion. Hence, add TODO to remove
    firmware_UpdateKernelSubkeyVersion when PSV testplans are done.
    
    BUG=b:355525422, b:194910869
    TEST=None
    
    Change-Id: Iea2c3bf80307fabdb4a7eb056ecdd115571e58b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/5740918
    Tested-by: Peggy Chen <peggy.chen@cienet.com>
    Commit-Queue: Peggy Chen <peggy.chen@cienet.com>
    Reviewed-by: Jeremy Bettis <jbettis@chromium.org>

--- server/site_tests/firmware_UpdateKernelSubkeyVersion/firmware_UpdateKernelSubkeyVersion.py
+++ server/site_tests/firmware_UpdateKernelSubkeyVersion/firmware_UpdateKernelSubkeyVersion.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# TODO(b/355525422): Remove from PVS testplans and remove this file.
+
 import logging
 import os
 

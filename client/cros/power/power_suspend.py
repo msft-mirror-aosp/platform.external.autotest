642750b7de4a4dc4dde0c5f61374d900ed3c45fc
Vidya Gopalakrishnan <vidya.gopalakrishnan@intel.com>
2025-01-09 10:23:50 +0530

    power_suspend: Ignore double parenthesis '{}' while parsing device times
    ISH device name in kernel log contains double parenthesis which is not
    processed by the power_resume script. Hence this needs to be removed in
    power_suspend.py while parsing the device resume times
    
    BUG=b:382421473
    TEST=Build and test on device
    cros workon --board=nissa start chromeos-base/autotest
    emerge-nissa autotest
    test_that -b nissa <Device IP address> f:.*power_Resume/control
    - verified that ISH devices are present in
    test_that_logs\results-1-power_Resume\power_Resume\results\keyval file when ISH is one of the 10 slowest devices to resume.
    
    Change-Id: If241185b08835b1267c81f5df6583bf8da346378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/6161675
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Nancy Zhao <zhaon@google.com>
    Tested-by: Vidya Gopalakrishnan <vidya.gopalakrishnan@intel.corp-partner.google.com>
    Commit-Queue: Vidya Gopalakrishnan <vidya.gopalakrishnan@intel.corp-partner.google.com>

--- client/cros/power/power_suspend.py
+++ client/cros/power/power_suspend.py
@@ -419,13 +419,15 @@ class Suspender(object):
         # Need to parse device and time taken. ex log below
         # 2022-02-09T19:34:11.295506Z INFO kernel: [   90.035439] ieee80211 phy0: wiphy_resume+0x0/0x138 [cfg80211] returned 0 after 47911 usecs
         # 2022-07-18T08:23:55.328597Z INFO kernel: [ 1093.127017] xhci_hcd 0000:04:00.4: PM: pci_pm_suspend+0x0/0x1f1 returned 0 after 873 usecs
-        # Identify "ieee80211 phy0/xhci_hcd 0000:04:00.4" and "47911/873" using below regex
+        # 2024-12-03T08:50:28.217419Z INFO kernel: [  161.477557] cros_ec_ishtp {7B7154D0-56F4-4BDC-B0D8-9E7CDAE0D6A0}: PM: ishtp_cl_device_resume+0x0/0x40 [intel_ishtp] returned 0 after 24891 usecs
+        # Identify "ieee80211 phy0/xhci_hcd/ 0000:04:00.4/cros_ec_ishtp {7B7154D0-56F4-4BDC-B0D8-9E7CDAE0D6A0}" and "47911/873/24891" using below regex
         regex = re.compile(r'] (.*): .* returned 0 after ([0-9]+) usecs')
         phase_table = self._get_phase_times()
         for line in self._logs:
             match = regex.search(line)
             if match:
-                device = match.group(1).replace(' ', '_').replace(':','-')
+                device = match.group(1).replace(' ', '_').replace(
+                        ':', '-').replace('{', '').replace('}', '')
                 key = 'seconds_dev_' + device
                 secs = float(match.group(2)) / 1e6
                 ts = cros_logging.extract_kernel_timestamp(line)

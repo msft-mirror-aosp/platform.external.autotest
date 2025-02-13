53ec1017ba932b1d2f67cea7a57e76fb8e869004
vivianchen <vivian.chen@cienet.com>
2025-02-13 17:47:03 +0800

    power_BrightnessResetAfterReboot: Add back ALS consideration
    On some models, ALS might have already changed the brightness during
    boot, and disabling powerd cannot help with this issue. Add the ALS
    consideration logic back while still keeping stop powerd action for
    eve devices.
    
    BUG=b:394100008
    TEST=test_that $DUT power_BrightnessResetAfterReboot
    
    Change-Id: If9e669152f21fcca3cd1443304ff63ce9e59b7fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/6263226
    Tested-by: Vivian Chen <vivian.chen@cienet.com>
    Reviewed-by: Mengqi Guo <mqg@chromium.org>
    Reviewed-by: Jing Li <jingmuli@google.com>
    Commit-Queue: Vivian Chen <vivian.chen@cienet.com>

--- server/site_tests/power_BrightnessResetAfterReboot/power_BrightnessResetAfterReboot.py
+++ server/site_tests/power_BrightnessResetAfterReboot/power_BrightnessResetAfterReboot.py
@@ -3,8 +3,18 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+
 from autotest_lib.client.common_lib import error
 from autotest_lib.server import autotest, test
+
+
+# TODO(b/163372205): Some models before unibuild have ambient light sensors
+# only on certain skus. However, has_ambient_light_sensor powerd pref has to
+# be set for all skus. For these models, skip checking the existence of ambient
+# light sensors against has_ambient_light_sensor powerd pref.
+IGNORE_ALS_PREF_MODELS = ['caroline']
+
 
 class power_BrightnessResetAfterReboot(test.test):
     """Verifies that the panel brightness level resets after device reboots.
@@ -24,10 +34,40 @@ class power_BrightnessResetAfterReboot(test.test):
         disable_powerd(host)
         autotest_client.run_test(client_autotest, exit_without_logout=True)
 
+        cmd = 'check_powerd_config --ambient_light_sensor'
+        num_als_pref = int(host.run_output(cmd, ignore_status=True))
+
+        cmd = 'backlight_tool --get_ambient_light_lux'
+        result = host.run(cmd, ignore_status=True)
+        als_exists = not result.exit_status  # 0 is cmd success.
+
+        if num_als_pref and not als_exists:
+            model = host.get_platform()
+            msg = ('Powerd pref indicates %d ambient light sensor(s) but device '
+                   'is unable to find it (them).' % num_als_pref)
+            if model in IGNORE_ALS_PREF_MODELS:
+                logging.info('%s However, skip this check for model: %s.', msg,
+                             model)
+            else:
+                raise error.TestFail(msg)
+
+        initial_lux = -1
+        if als_exists:
+            initial_lux = int(result.stdout.rstrip())
+            cushion_percent = 5
+            brightness_range = \
+                    [get_backlight(host, initial_lux / 2) - cushion_percent,
+                    get_backlight(host, initial_lux * 2) + cushion_percent]
+        else:
+            brightness_range = [10.0, 90.0]
+
         initial_brightness = get_backlight(host)
-        if (initial_brightness < 10.0 or initial_brightness > 90.0):
+        if (initial_brightness < brightness_range[0]
+                    or initial_brightness > brightness_range[1]):
             raise error.TestFail('Default brightness level is out of scope '
-                                 '(10.0%% - 90.0%%): %f' % initial_brightness)
+                                 '(%d%% - %d%%): %f' %
+                                 (brightness_range[0], brightness_range[1],
+                                  initial_brightness))
 
         brightness_min = 0.0
         if (not set_backlight(host, brightness_min) or
@@ -43,20 +83,34 @@ class power_BrightnessResetAfterReboot(test.test):
 
         host.reboot()
         # Disable powerd to ensure ALS doesn't affect brightness during testing.
+        # See b/378347551 for for additional details.
         disable_powerd(host)
         autotest_client.run_test(client_autotest, exit_without_logout=True)
         brightness_after_reboot = get_backlight(host)
-        if not initial_brightness == brightness_after_reboot:
-            raise error.TestFail(
-                    'Unable to reset internal display brightness back '
-                    'to default after reboot.\n'
-                    'Previous boot default brightness: %f\n'
-                    'Current boot default brightness: %f' %
-                    (initial_brightness, brightness_after_reboot))
+        if not als_exists and initial_brightness == brightness_after_reboot:
+            return
+        # If there is an ambient light sensor, allow a small change in internal
+        # display brightness, in case that the ambient light changes slightly.
+        if als_exists:
+            cushion_lux = 0.2
+            cushion_percent = 1
+            lux_after_reboot = [(1.0 - cushion_lux) * initial_lux,
+                                (1.0 + cushion_lux) * initial_lux]
+            brightness_range_after_reboot = [
+                    get_backlight(host, lux_after_reboot[0]) - cushion_percent,
+                    get_backlight(host, lux_after_reboot[1]) + cushion_percent
+            ]
+            if (brightness_range_after_reboot[0] <= brightness_after_reboot <=
+                        brightness_range_after_reboot[1]):
+                return
 
-    def cleanup(self, host):
-        """Cleanup the test"""
-        host.run('start powerd')
+        raise error.TestFail(
+                'Unable to reset internal display brightness back '
+                'to default after reboot.\n'
+                'Previous boot default brightness: %f\n'
+                'Current boot default brightness: %f' %
+                (initial_brightness, brightness_after_reboot))
+
 
 def set_backlight(host, percentage):
     """Executes backlight_tool to set internal display backlight.
@@ -72,11 +126,30 @@ def set_backlight(host, percentage):
     return not exit_status # 0 is cmd success.
 
 
-def get_backlight(host):
+def get_backlight(host, lux=-1):
     """Executes backlight_tool to get internal display backlight.
        @param host: host object representing the DUT.
     """
     cmd = 'backlight_tool --get_brightness_percent'
+    if lux >= 0:
+        cmd = 'backlight_tool --get_initial_brightness --lux=%d' % lux
+    try:
+        result = host.run_output(cmd)
+    except error.CmdError:
+        raise error.TestFail(cmd)
+
+    result = float(result)
+    if lux >= 0:
+        result = level_to_linear(host, result)
+    return result
+
+
+def level_to_linear(host, level):
+    """Executes backlight_tool to convert the provided brightness level to linear.
+       @param host: host object representing the DUT.
+       @param level: brightness level to convert to.
+    """
+    cmd = 'backlight_tool --level_to_linear=%d' % level
     try:
         result = host.run_output(cmd)
     except error.CmdError:
